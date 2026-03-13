de71c9c8470680825e91522a6b4fa1159ebc3c1d
Pari Goyal <112866509+PariGoyal27@users.noreply.github.com>
2026-03-13 09:25:09 +0530

    Time: 0 ms (100%), Space: 41.7 MB (96.44%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,15 +1,28 @@
+// Recursion + memoization
+// class Solution {
+//     int[] dp = new int[46];
+//     private int solve(int n){
+//         if(n == 0){
+//             return 1;
+//         }else if(n < 0) return 0;
+//         if(dp[n] != -1) return dp[n];
+//         dp[n] = solve(n-1) + solve(n-2);
+//         return dp[n];
+//     }
+//     public int climbStairs(int n) {
+//         Arrays.fill(dp, -1);
+//         return solve(n);
+//     }
+// }
+
+//memoization dp
 class Solution {
-    int[] dp = new int[46];
-    private int solve(int n){
-        if(n == 0){
-            return 1;
-        }else if(n < 0) return 0;
-        if(dp[n] != -1) return dp[n];
-        dp[n] = solve(n-1) + solve(n-2);
-        return dp[n];
-    }
     public int climbStairs(int n) {
-        Arrays.fill(dp, -1);
-        return solve(n);
+        int[] dp = new int[n+1];
+        dp[0] = 1; dp[1] = 1;
+        for(int i = 2; i < n+1; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 }
