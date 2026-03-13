58627e056013a28dd44d294b9fc7f873e090ec15
Pari Goyal <112866509+PariGoyal27@users.noreply.github.com>
2026-03-13 09:32:46 +0530

    Time: 0 ms (100%), Space: 41.9 MB (92.34%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,13 +1,26 @@
+//Recursion + memoization
+// class Solution {
+//     int[] dp = new int[31];
+//     private int solve(int n){
+//         if(n == 0 || n == 1) return n;
+//         if(dp[n] != -1) return dp[n];
+//         dp[n] = solve(n-1) + solve(n-2);
+//         return dp[n];
+//     }
+//     public int fib(int n) {
+//         Arrays.fill(dp, -1);
+//         return solve(n);
+//     }
+// }
+//Memoization + dp
 class Solution {
-    int[] dp = new int[31];
-    private int solve(int n){
-        if(n == 0 || n == 1) return n;
-        if(dp[n] != -1) return dp[n];
-        dp[n] = solve(n-1) + solve(n-2);
-        return dp[n];
-    }
     public int fib(int n) {
-        Arrays.fill(dp, -1);
-        return solve(n);
+        if(n == 0 || n == 1) return n;
+        int[] dp = new int[n+1];
+        dp[0] = 0; dp[1] = 1;
+        for(int i = 2; i < n+1; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 }
