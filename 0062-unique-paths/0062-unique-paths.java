1c11ed38402e13d85d03c5adb3a0f70c9a2e08cd
Pari Goyal <112866509+PariGoyal27@users.noreply.github.com>
2026-03-19 14:18:57 +0530

    Time: 1 ms (14.04%), Space: 42.2 MB (41.58%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -21,7 +21,6 @@ class Solution {
 //     int[][] dp = new int[101][101];
 //     private int solve(int i, int j, int m, int n){
 //         if(i == m-1 || j == n-1) return 1;
-//         // if(i > m || j > n) return 0;
 //         if(dp[i][j] != -1) return dp[i][j];
 //         int down = solve(i+1, j, m, n);
 //         int right = solve(i, j+1, m, n);
@@ -36,7 +35,6 @@ class Solution {
 // class Solution {
 //     private int solve(int i, int j, int m, int n){
 //         if(i == m-1 || j == n-1) return 1;
-//         // if(i > m || j > n) return 0;
 //         int down = solve(i+1, j, m, n);
 //         int right = solve(i, j+1, m, n);
 //         return down + right;
