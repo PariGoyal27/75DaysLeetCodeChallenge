c1fe5508c406f9bdf6f8e344c68f3268bb17e5ba
Pari Goyal <112866509+PariGoyal27@users.noreply.github.com>
2026-03-19 16:39:20 +0530

    Time: 5 ms (24.77%), Space: 50.4 MB (33.92%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,27 +1,49 @@
-//Recursion + memoization
+//Bottom up
 class Solution {
-    int sum = 0;
-    private int solve(int[][] grid, int i, int j, int m, int n, int[][] dp){
-        if(i == m-1 && j == n-1)
-            return grid[i][j];
-        if(i >= m || j >= n)
-            return Integer.MAX_VALUE;
-        if(dp[i][j] != -1) return dp[i][j];
-        int down = solve(grid, i+1, j, m, n, dp);
-        int right = solve(grid, i, j+1, m, n, dp);
-        return dp[i][j] = grid[i][j] + Math.min(down, right);
-    }
     public int minPathSum(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
         int[][] dp = new int[m][n];
-        for(int[] row : dp) Arrays.fill(row, -1);
-        return solve(grid, 0, 0, m, n, dp);
+        dp[0][0] = grid[0][0];
+        //First Column
+        for(int i = 1; i < m; i++){
+            dp[i][0] = grid[i][0] + dp[i-1][0];
+        }
+        //First Row
+        for(int j = 1; j < n; j++){
+            dp[0][j] = grid[0][j] + dp[0][j-1];
+        }
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                dp[i][j] = grid[i][j] + Math.min(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        return dp[m-1][n-1];
     }
 }
+
+//Recursion + memoization
+// class Solution {
+//     private int solve(int[][] grid, int i, int j, int m, int n, int[][] dp){
+//         if(i == m-1 && j == n-1)
+//             return grid[i][j];
+//         if(i >= m || j >= n)
+//             return Integer.MAX_VALUE;
+//         if(dp[i][j] != -1) return dp[i][j];
+//         int down = solve(grid, i+1, j, m, n, dp);
+//         int right = solve(grid, i, j+1, m, n, dp);
+//         return dp[i][j] = grid[i][j] + Math.min(down, right);
+//     }
+//     public int minPathSum(int[][] grid) {
+//         int m = grid.length;
+//         int n = grid[0].length;
+//         int[][] dp = new int[m][n];
+//         for(int[] row : dp) Arrays.fill(row, -1);
+//         return solve(grid, 0, 0, m, n, dp);
+//     }
+// }
 //Recursion
 // class Solution {
-//     int sum = 0;
 //     private int solve(int[][] grid, int i, int j, int m, int n){
 //         if(i == m-1 && j == n-1)
 //             return grid[i][j];
