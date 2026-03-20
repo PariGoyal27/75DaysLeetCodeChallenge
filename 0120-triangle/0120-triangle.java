7f1e5158f302e0fbbd7779a5bbbb568faf31119c
Pari Goyal <112866509+PariGoyal27@users.noreply.github.com>
2026-03-20 20:18:22 +0530

    Time: 3 ms (83.66%), Space: 45.6 MB (59.47%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,30 +1,47 @@
-//Memo + recursion TLE -> 27/46
+//Bottom up
 class Solution {
-     private int solve(List<List<Integer>> list, int i, int j, Integer[][] dp){
-        int m = list.size();
-        // if(j > i || i >= m) // No need boundry Checks because it won't go there  
-        //     return Integer.MAX_VALUE-1;
-        if(i == m-1)
-            return dp[i][j] = list.get(i).get(j);
-        if(dp[i][j] != null) return dp[i][j];
-        int down = solve(list, i+1, j, dp);
-        int right = solve(list, i+1, j+1, dp);
-        return  dp[i][j] = list.get(i).get(j) + Math.min(down, right);
-    }
     public int minimumTotal(List<List<Integer>> triangle) {
         int n = triangle.size();
-        Integer[][] dp = new Integer[n][n];
-        // for(int[] row : dp)
-        //     Arrays.fill(row, -1);
-        return solve(triangle, 0, 0, dp);
+        int[][] dp = new int[n][n];
+
+        //Copy last row
+        for(int j = 0; j < n; j++){
+            dp[n-1][j] = triangle.get(n-1).get(j);
+        }
+        //Go Upwards
+        for(int i = n-2; i >= 0; i--){
+            for(int j = 0; j <= i; j++){
+                dp[i][j] = triangle.get(i).get(j) + Math.min(dp[i+1][j], dp[i+1][j+1]);
+            }
+        }
+        return dp[0][0];
     }
 }
+//Memo + recursion Integer not int because list is of type Integer
+// class Solution {
+//      private int solve(List<List<Integer>> list, int i, int j, Integer[][] dp){
+//         int m = list.size();
+//         // if(j > i || i >= m) // No need boundry Checks because it won't go there  
+//         //     return Integer.MAX_VALUE-1;
+//         if(i == m-1)
+//             return dp[i][j] = list.get(i).get(j);
+//         if(dp[i][j] != null) return dp[i][j];
+//         int down = solve(list, i+1, j, dp);
+//         int right = solve(list, i+1, j+1, dp);
+//         return  dp[i][j] = list.get(i).get(j) + Math.min(down, right);
+//     }
+//     public int minimumTotal(List<List<Integer>> triangle) {
+//         int n = triangle.size();
+//         Integer[][] dp = new Integer[n][n];
+//         // for(int[] row : dp)
+//         //     Arrays.fill(row, -1);
+//         return solve(triangle, 0, 0, dp);
+//     }
+// }
 //Recursion TLE -> 27/46
 // class Solution {
 //      private int solve(List<List<Integer>> list, int i, int j){
 //         int m = list.size();
-//         if(j > i || i >= m)
-//             return Integer.MAX_VALUE;
 //         if(i == m-1)
 //             return list.get(i).get(j);
 //         int down = solve(list, i+1, j);
