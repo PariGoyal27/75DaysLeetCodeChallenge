684b474a327327518d1564757720a0f6aa46a014
Pari Goyal <112866509+PariGoyal27@users.noreply.github.com>
2026-03-22 10:53:49 +0530

    Time: 619 ms (31.06%), Space: 155.4 MB (11.89%) - LeetHub

---
+++ 3877-minimum-removals-to-achieve-target-xor/3877-minimum-removals-to-achieve-target-xor.java
@@ -0,0 +1,43 @@
+// Recursion + Memoization
+class Solution {
+    HashMap<String, Integer> map = new HashMap<>();
+    private int solve(int[] nums, int xor, int i, int target){
+        if(i == nums.length){
+            if(xor == target) return 0;
+            return Integer.MAX_VALUE;
+        }
+        String temp = i + " " + xor;
+        if(map.containsKey(temp)) return map.get(temp);
+
+        int take = solve(nums, xor ^ nums[i], i+1, target);
+        int notTake = solve(nums, xor, i+1, target);
+        if(notTake != Integer.MAX_VALUE) notTake += 1;
+        int ans = Math.min(take, notTake);
+        map.put(temp, ans);
+        return ans;
+    }
+    public int minRemovals(int[] nums, int target) {
+        int n = nums.length;
+        map.clear();
+        int ans = solve(nums, 0, 0, target);
+        return ans == Integer.MAX_VALUE ? -1 : ans;
+    }
+}
+// Recursion
+// class Solution {
+//     private int solve(int[] nums, int xor, int i, int target){
+//         if(i == nums.length){
+//             if(xor == target) return 0;
+//             return Integer.MAX_VALUE;
+//         }
+//         int take = solve(nums, xor ^ nums[i], i+1, target);
+//         int notTake = solve(nums, xor, i+1, target);
+//         if(notTake != Integer.MAX_VALUE) notTake += 1;
+//         return Math.min(take, notTake);
+//     }
+//     public int minRemovals(int[] nums, int target) {
+//         int n = nums.length;
+//         int ans = solve(nums, 0, 0, target);
+//         return ans == Integer.MAX_VALUE ? -1 : ans;
+//     }
+// }
