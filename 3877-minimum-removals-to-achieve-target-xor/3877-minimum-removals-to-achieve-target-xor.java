19e43c80062a659ef1faa0d95b33528a11db00fd
Pari Goyal <112866509+PariGoyal27@users.noreply.github.com>
2026-03-22 11:17:44 +0530

    Time: 704 ms (26.96%), Space: 274.8 MB (5.47%) - LeetHub

--- 3877-minimum-removals-to-achieve-target-xor/3877-minimum-removals-to-achieve-target-xor.java
+++ 3877-minimum-removals-to-achieve-target-xor/3877-minimum-removals-to-achieve-target-xor.java
@@ -1,29 +1,54 @@
-// Recursion + Memoization
 class Solution {
-    HashMap<String, Integer> map = new HashMap<>();
+    int[][] dp = new int[41][20000];
     private int solve(int[] nums, int xor, int i, int target){
         if(i == nums.length){
             if(xor == target) return 0;
             return Integer.MAX_VALUE;
         }
-        String temp = i + " " + xor;
-        if(map.containsKey(temp)) return map.get(temp);
+        
+        if(dp[i][xor] != -1) return dp[i][xor];
 
         int take = solve(nums, xor ^ nums[i], i+1, target);
         int notTake = solve(nums, xor, i+1, target);
         if(notTake != Integer.MAX_VALUE) notTake += 1;
         int ans = Math.min(take, notTake);
-        map.put(temp, ans);
+        dp[i][xor] = ans;
         return ans;
     }
     public int minRemovals(int[] nums, int target) {
         int n = nums.length;
-        map.clear();
+        // map.clear();
+        for(int[] row : dp) Arrays.fill(row, -1);
         int ans = solve(nums, 0, 0, target);
         return ans == Integer.MAX_VALUE ? -1 : ans;
     }
 }
-// Recursion
+// Recursion + Memoization - worked
+// class Solution {
+//     HashMap<String, Integer> map = new HashMap<>();
+//     private int solve(int[] nums, int xor, int i, int target){
+//         if(i == nums.length){
+//             if(xor == target) return 0;
+//             return Integer.MAX_VALUE;
+//         }
+//         String temp = i + " " + xor;
+//         if(map.containsKey(temp)) return map.get(temp);
+
+//         int take = solve(nums, xor ^ nums[i], i+1, target);
+//         int notTake = solve(nums, xor, i+1, target);
+//         if(notTake != Integer.MAX_VALUE) notTake += 1;
+//         int ans = Math.min(take, notTake);
+//         map.put(temp, ans);
+//         return ans;
+//     }
+//     public int minRemovals(int[] nums, int target) {
+//         int n = nums.length;
+//         map.clear();
+//         int ans = solve(nums, 0, 0, target);
+//         return ans == Integer.MAX_VALUE ? -1 : ans;
+//     }
+// }
+// Recursion - TLE(972/1048)
 // class Solution {
 //     private int solve(int[] nums, int xor, int i, int target){
 //         if(i == nums.length){
