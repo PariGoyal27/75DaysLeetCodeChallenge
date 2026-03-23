9f31a8f8019b19fa27de823b83c010ab637d793f
Pari Goyal <112866509+PariGoyal27@users.noreply.github.com>
2026-03-23 19:24:07 +0530

    Time: 14 ms (93.61%), Space: 51.7 MB (14.05%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g); Arrays.sort(s);
+        int i = 0, j = 0;
+        // int cnt = 0;
+        while(i < s.length && j < g.length){
+            if(s[i] >= g[j]) {
+                j++;
+                // cnt++;
+            }
+            i++;
+        }
+        return j;
+    }
+}
