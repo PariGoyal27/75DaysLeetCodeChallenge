49ce3118aa318323f9e7f90b992732ec139a5501
Pari Goyal <112866509+PariGoyal27@users.noreply.github.com>
2026-03-24 14:58:46 +0530

    Time: 0 ms (100%), Space: 42.8 MB (82.37%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.java
+++ 1328-break-a-palindrome/1328-break-a-palindrome.java
@@ -3,17 +3,13 @@ class Solution {
         int n = palindrome.length();
         if(n == 1) return "";
         StringBuilder sb = new StringBuilder(palindrome);
-        boolean allA = true;
         for(int i = 0; i < n/2; i++){
             if(sb.charAt(i) != 'a'){
                 sb.setCharAt(i, 'a');
-                allA = false;
-                break;
+                return sb.toString();
             }
         }
-        if(allA){
-            sb.setCharAt(n-1, 'b');
-        }
+        sb.setCharAt(n-1, 'b');
         return sb.toString();
     }
 }
