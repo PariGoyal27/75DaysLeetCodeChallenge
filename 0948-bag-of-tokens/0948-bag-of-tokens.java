1eb04ee1880647a74284e4a09fc04e110dce890e
Pari Goyal <112866509+PariGoyal27@users.noreply.github.com>
2026-03-24 12:45:01 +0530

    Time: 5 ms (99.42%), Space: 44.8 MB (96.16%) - LeetHub

--- 0948-bag-of-tokens/0948-bag-of-tokens.java
+++ 0948-bag-of-tokens/0948-bag-of-tokens.java
@@ -4,6 +4,10 @@ class Solution {
         int n = tokens.length;
         int l = 0, r = n-1;
         int scr = 0, maxScore = 0;
+        /*greed -> maximize power to increase score
+        maximize power -> playing last(highest) token(power jyada se jyada bdh jaaye)
+        if(that is not working) minimize power -> playing smallest token(jisse power jyada km na ho)
+        */
         while(l <= r){
             if(power >= tokens[l]){
                 power -= tokens[l];
