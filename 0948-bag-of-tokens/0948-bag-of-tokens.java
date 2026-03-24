49acd9949cd57a119ac7cb82c7d56b1983496669
Pari Goyal <112866509+PariGoyal27@users.noreply.github.com>
2026-03-24 12:42:50 +0530

    Time: 5 ms (99.42%), Space: 45.4 MB (15.62%) - LeetHub

---
+++ 0948-bag-of-tokens/0948-bag-of-tokens.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int bagOfTokensScore(int[] tokens, int power) {
+        Arrays.sort(tokens);
+        int n = tokens.length;
+        int l = 0, r = n-1;
+        int scr = 0, maxScore = 0;
+        while(l <= r){
+            if(power >= tokens[l]){
+                power -= tokens[l];
+                l++;
+                scr++;
+                maxScore = Math.max(scr, maxScore);
+            }else if(scr >= 1){
+                power += tokens[r];
+                r--;
+                scr--;
+            }else
+                break;
+        }
+        return maxScore;
+    }
+}
