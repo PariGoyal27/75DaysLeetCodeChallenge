783d04ba3822cbc036729f8069618681f5babdc8
Pari Goyal <112866509+PariGoyal27@users.noreply.github.com>
2026-03-26 18:02:13 +0530

    Time: 2 ms (70.19%), Space: 45.5 MB (34.63%) - LeetHub

---
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -0,0 +1,61 @@
+class Solution {
+    public int maxProfit(int k, int[] prices) {
+        int n = prices.length;
+        int[][][] dp = new int[n+1][2][k+1];
+        for(int i = n-1; i >= 0; i--){
+            for(int buy = 0; buy <= 1; buy++){
+                for(int cap = 1; cap <= k; cap++){
+                    if(buy == 1)
+                        dp[i][buy][cap] = Math.max(-prices[i] + dp[i+1][0][cap], dp[i+1][1][cap]);
+                    else
+                        dp[i][buy][cap] = Math.max(prices[i] + dp[i+1][1][cap-1], dp[i+1][0][cap]);
+                }
+            }
+        }
+        return dp[0][1][k];
+    }
+}
+//memoization
+// class Solution{
+//     public int maxProfit(int k, int[] prices){
+//         int n = prices.length;
+//         int[][][] dp = new int[n + 1][2][k + 1];
+//         for(int[][] mat : dp){
+//             for(int[] row : mat){
+//                 Arrays.fill(row, -1);
+//             }
+//         }
+//         return solve(0, 1, k, n, prices, dp);
+//     }
+//     public int solve(int ind, int buy, int k, int n, int[] prices, int[][][] dp){
+//         if(ind == n || k == 0) return 0;
+//         if(dp[ind][buy][k] != -1) return dp[ind][buy][k];
+//         int profit = 0;
+//         if(buy == 1){
+//             profit = Math.max(-prices[ind] + solve(ind + 1, 0, k, n, prices, dp), 0 + solve(ind + 1, 1, k, n, prices, dp));
+//         }else{
+//             profit = Math.max(prices[ind] + solve(ind + 1, 1, k - 1, n, prices, dp), 0 + solve(ind + 1, 0, k, n, prices, dp));
+//         }
+//         dp[ind][buy][k] = profit;
+//         return profit;
+//     }
+// }
+
+
+//recursive
+// class Solution {
+//     public int maxProfit(int k, int[] prices) {
+//         int n = prices.length;
+//         return solve(0, 1, k, n, prices);
+//     }
+//     public int solve(int ind, int buy, int k, int n, int[] prices){
+//         if(ind == n || k == 0) return 0;
+//         int profit = 0;
+//         if(buy == 1){
+//             profit = Math.max(-prices[ind] + solve(ind + 1, 0, k, n, prices), 0 + solve(ind + 1, 1, k, n, prices));
+//         }else{
+//             profit = Math.max(prices[ind] + solve(ind + 1, 1, k - 1, n, prices), 0 + solve(ind + 1, 0, k, n, prices));
+//         }
+//         return profit;
+//     }
+// }
