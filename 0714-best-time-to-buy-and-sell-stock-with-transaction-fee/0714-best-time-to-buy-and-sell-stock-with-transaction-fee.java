f9da426fcde6b6a9c51cf107d9466d64f0d6947a
Pari Goyal <112866509+PariGoyal27@users.noreply.github.com>
2026-03-26 18:41:42 +0530

    Time: 5 ms (84.59%), Space: 64.9 MB (96.23%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,19 +1,37 @@
-//Tabulation
+//Space Optimization
 class Solution {
     public int maxProfit(int[] prices, int fee) {
         int n = prices.length;
-        int[][] dp = new int[n+1][2];
+        int aheadNotBuy = 0, aheadBuy = 0;
+        int currNotBuy = 0, currBuy = 0;
         for(int i = n-1; i >= 0; i--){
-            // for(int buy = 0; buy <= 1; buy++){
-                // if(buy == 1)
-                    dp[i][1] = Math.max(-prices[i] - fee + dp[i+1][0], dp[i+1][1]);
-                // else
-                    dp[i][0] = Math.max(prices[i] + dp[i+1][1], dp[i+1][0]);   
-            // }
+            currBuy = Math.max(-prices[i] - fee + aheadNotBuy,
+                                aheadBuy);
+            currNotBuy = Math.max(prices[i] + aheadBuy,
+                            aheadNotBuy);
+            aheadBuy = currBuy;
+            aheadNotBuy = currNotBuy;
         }
-        return dp[0][1];
+        return aheadBuy;
     }
 }
+
+//Tabulation
+// class Solution {
+//     public int maxProfit(int[] prices, int fee) {
+//         int n = prices.length;
+//         int[][] dp = new int[n+1][2];
+//         for(int i = n-1; i >= 0; i--){
+//             // for(int buy = 0; buy <= 1; buy++){
+//                 // if(buy == 1)
+//                     dp[i][1] = Math.max(-prices[i] - fee + dp[i+1][0], dp[i+1][1]);
+//                 // else
+//                     dp[i][0] = Math.max(prices[i] + dp[i+1][1], dp[i+1][0]);   
+//             // }
+//         }
+//         return dp[0][1];
+//     }
+// }
 //Recursion + Memoization
 // class Solution {
 //     private int solve(int i, int buy, int[] prices, int fee, int[][] dp){
