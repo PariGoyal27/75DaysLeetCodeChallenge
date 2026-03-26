be62b1187880e89d9ca3b48a64c7d37fb63dfdb8
Pari Goyal <112866509+PariGoyal27@users.noreply.github.com>
2026-03-26 18:37:35 +0530

    Time: 18 ms (43.3%), Space: 70.6 MB (52.04%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,19 +1,36 @@
+//Tabulation
 class Solution {
-    private int solve(int i, int buy, int[] prices, int fee, int[][] dp){
-        if(i == prices.length) return 0;
-
-        if(dp[i][buy] != -1) return dp[i][buy];
-
-        if(buy == 1)
-            return dp[i][buy] = Math.max(-prices[i] - fee + solve(i+1, 0, prices, fee, dp), solve(i+1, 1, prices, fee, dp));
-        else
-            return dp[i][buy] = Math.max(prices[i] + solve(i+1, 1, prices, fee, dp), solve(i+1, 0, prices, fee, dp));
-    }
     public int maxProfit(int[] prices, int fee) {
         int n = prices.length;
         int[][] dp = new int[n+1][2];
-        for(int[] row : dp)
-            Arrays.fill(row, -1);
-        return solve(0, 1, prices, fee, dp);
+        for(int i = n-1; i >= 0; i--){
+            // for(int buy = 0; buy <= 1; buy++){
+                // if(buy == 1)
+                    dp[i][1] = Math.max(-prices[i] - fee + dp[i+1][0], dp[i+1][1]);
+                // else
+                    dp[i][0] = Math.max(prices[i] + dp[i+1][1], dp[i+1][0]);   
+            // }
+        }
+        return dp[0][1];
     }
 }
+//Recursion + Memoization
+// class Solution {
+//     private int solve(int i, int buy, int[] prices, int fee, int[][] dp){
+//         if(i == prices.length) return 0;
+
+//         if(dp[i][buy] != -1) return dp[i][buy];
+
+//         if(buy == 1)
+//             return dp[i][buy] = Math.max(-prices[i] - fee + solve(i+1, 0, prices, fee, dp), solve(i+1, 1, prices, fee, dp));
+//         else
+//             return dp[i][buy] = Math.max(prices[i] + solve(i+1, 1, prices, fee, dp), solve(i+1, 0, prices, fee, dp));
+//     }
+//     public int maxProfit(int[] prices, int fee) {
+//         int n = prices.length;
+//         int[][] dp = new int[n+1][2];
+//         for(int[] row : dp)
+//             Arrays.fill(row, -1);
+//         return solve(0, 1, prices, fee, dp);
+//     }
+// }
