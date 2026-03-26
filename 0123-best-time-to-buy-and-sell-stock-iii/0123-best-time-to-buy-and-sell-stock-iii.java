eb980f1d68843edeff0aae36193157ac2855c074
Pari Goyal <112866509+PariGoyal27@users.noreply.github.com>
2026-03-26 19:07:19 +0530

    Time: 13 ms (45.15%), Space: 76.1 MB (58.94%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,23 +1,49 @@
+//Space Optimization
 class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        int[][][] dp = new int[n+1][2][3];
+        int[][] ahead = new int[2][3];
+        int[][] curr = new int[2][3];
         for(int i = n-1; i >= 0; i--){
-            for(int buy = 0; buy <= 1; buy++){
+            // for(int buy = 0; buy <= 1; buy++){
                 for(int cap = 1; cap <= 2; cap++){
-                    if(buy == 1){
-                        dp[i][buy][cap] = Math.max(-prices[i] + dp[i+1][0][cap], 
-                                0 + dp[i+1][1][cap]);
-                    }else{ 
-                        dp[i][buy][cap] = Math.max(prices[i] + dp[i+1][1][cap-1], 
-                                0 + dp[i+1][0][cap]);
-                    }
+                    // if(buy == 1){
+                        curr[1][cap] = Math.max(-prices[i] + ahead[0][cap], 
+                                0 + ahead[1][cap]);
+                    // }else{ 
+                        curr[0][cap] = Math.max(prices[i] + ahead[1][cap-1], 
+                                0 + ahead[0][cap]);
+                    // }
                 }
+            // }
+            for(int j = 0; j < 2; j++){
+                System.arraycopy(curr[j], 0, ahead[j], 0, 3);
             }
         }
-        return dp[0][1][2];
+        return ahead[1][2];
     }
 }
+//Tabulation
+// class Solution {
+//     public int maxProfit(int[] prices) {
+//         int n = prices.length;
+//         int[][][] dp = new int[n+1][2][3];
+//         for(int i = n-1; i >= 0; i--){
+//             // for(int buy = 0; buy <= 1; buy++){
+//                 for(int cap = 1; cap <= 2; cap++){
+//                     // if(buy == 1){
+//                         dp[i][1][cap] = Math.max(-prices[i] + dp[i+1][0][cap], 
+//                                 0 + dp[i+1][1][cap]);
+//                     // }else{ 
+//                         dp[i][0][cap] = Math.max(prices[i] + dp[i+1][1][cap-1], 
+//                                 0 + dp[i+1][0][cap]);
+//                     // }
+//                 }
+//             // }
+//         }
+//         return dp[0][1][2];
+//     }
+// }
 //Recursion + Memoization
 // class Solution {
 //     private int solve(int i, int buy, int cap, int[] prices, int[][][] dp){
