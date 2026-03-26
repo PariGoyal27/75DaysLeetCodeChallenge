49fd38c4eb92ea121de00176052c8a9621d5be60
Pari Goyal <112866509+PariGoyal27@users.noreply.github.com>
2026-03-26 13:04:59 +0530

    Time: 54 ms (35.33%), Space: 118 MB (28.02%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,25 +1,45 @@
-//Recursion + Memoization
 class Solution {
-    private int solve(int i, int buy, int cap, int[] prices, int[][][] dp){
-        if(i == prices.length || cap == 0) return 0;
-        if(dp[i][buy][cap] != -1) return dp[i][buy][cap];
-
-        int profit = 0;
-        if(buy == 1){//Can buy
-            profit = Math.max(-prices[i] + solve(i+1, 0, cap, prices, dp), 
-                     0 + solve(i+1, 1, cap, prices, dp));
-        }else{ // can't buy only sell
-            profit = Math.max(prices[i] + solve(i+1, 1, cap-1, prices, dp), 
-                     0 + solve(i+1, 0, cap, prices, dp));
-        }
-        return dp[i][buy][cap] = profit;
-    }
     public int maxProfit(int[] prices) {
         int n = prices.length;
         int[][][] dp = new int[n+1][2][3];
-        for(int[][] mat : dp)
-            for(int[] row : mat)
-                Arrays.fill(row, -1);
-        return solve(0, 1, 2, prices, dp);
+        for(int i = n-1; i >= 0; i--){
+            for(int buy = 0; buy <= 1; buy++){
+                for(int cap = 1; cap <= 2; cap++){
+                    if(buy == 1){
+                        dp[i][buy][cap] = Math.max(-prices[i] + dp[i+1][0][cap], 
+                                0 + dp[i+1][1][cap]);
+                    }else{ 
+                        dp[i][buy][cap] = Math.max(prices[i] + dp[i+1][1][cap-1], 
+                                0 + dp[i+1][0][cap]);
+                    }
+                }
+            }
+        }
+        return dp[0][1][2];
     }
 }
+//Recursion + Memoization
+// class Solution {
+//     private int solve(int i, int buy, int cap, int[] prices, int[][][] dp){
+//         if(i == prices.length || cap == 0) return 0;
+//         if(dp[i][buy][cap] != -1) return dp[i][buy][cap];
+
+//         int profit = 0;
+//         if(buy == 1){//Can buy
+//             profit = Math.max(-prices[i] + solve(i+1, 0, cap, prices, dp), 
+//                      0 + solve(i+1, 1, cap, prices, dp));
+//         }else{ // can't buy only sell
+//             profit = Math.max(prices[i] + solve(i+1, 1, cap-1, prices, dp), 
+//                      0 + solve(i+1, 0, cap, prices, dp));
+//         }
+//         return dp[i][buy][cap] = profit;
+//     }
+//     public int maxProfit(int[] prices) {
+//         int n = prices.length;
+//         int[][][] dp = new int[n+1][2][3];
+//         for(int[][] mat : dp)
+//             for(int[] row : mat)
+//                 Arrays.fill(row, -1);
+//         return solve(0, 1, 2, prices, dp);
+//     }
+// }
