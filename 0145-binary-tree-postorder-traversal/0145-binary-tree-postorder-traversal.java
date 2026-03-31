c479b0e4977ace49b6d01dbbe2c0cf1c53b0046a
Pari Goyal <112866509+PariGoyal27@users.noreply.github.com>
2026-03-31 12:55:34 +0530

    Time: 0 ms (100%), Space: 43.2 MB (42.1%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
@@ -13,34 +13,62 @@
  *     }
  * }
  */
-class Solution {
-    List<Integer> ans;
-    // Recursive
-    // private void postorder(TreeNode node){
-    //     if(node == null) return;
-    //     postorder(node.left);
-    //     postorder(node.right);
-    //     ans.add(node.val);
-    // }
+
+ class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
-        ans = new ArrayList<>();
-        // postorder(root);
-        //Iterative
+        List<Integer> ans = new ArrayList<>();
         if(root == null) return ans;
-        Stack<TreeNode> st1 = new Stack<>();
-        // Stack<Integer> st2 = new Stack<>();
-        st1.push(root);
-        while(!st1.isEmpty()){
-            TreeNode node = st1.pop();
-            // st2.push(node.val);
-            ans.add(node.val);
-            if(node.left != null) st1.push(node.left);
-            if(node.right != null) st1.push(node.right);
+        Stack<TreeNode> st = new Stack<>();
+        TreeNode curr = root;
+        while(curr != null || !st.isEmpty()){
+            if(curr != null){
+                st.push(curr);
+                curr = curr.left;
+            }else{
+                TreeNode temp = st.peek().right;
+                if(temp == null){
+                    temp = st.pop();
+                    ans.add(temp.val);
+                    while(!st.isEmpty() && temp == st.peek().right){
+                        temp = st.pop();
+                        ans.add(temp.val);
+                    }
+                }else{
+                    curr = temp;
+                }
+            }
         }
-        // while(!st2.isEmpty()){
-        //     ans.add(st2.pop());
-        // }
-        Collections.reverse(ans);
         return ans;
     }
 }
+// class Solution {
+//     List<Integer> ans;
+//     // Recursive
+//     // private void postorder(TreeNode node){
+//     //     if(node == null) return;
+//     //     postorder(node.left);
+//     //     postorder(node.right);
+//     //     ans.add(node.val);
+//     // }
+//     public List<Integer> postorderTraversal(TreeNode root) {
+//         ans = new ArrayList<>();
+//         // postorder(root);
+//         //Iterative
+//         if(root == null) return ans;
+//         Stack<TreeNode> st1 = new Stack<>();
+//         // Stack<Integer> st2 = new Stack<>();
+//         st1.push(root);
+//         while(!st1.isEmpty()){
+//             TreeNode node = st1.pop();
+//             // st2.push(node.val);
+//             ans.add(node.val);
+//             if(node.left != null) st1.push(node.left);
+//             if(node.right != null) st1.push(node.right);
+//         }
+//         // while(!st2.isEmpty()){
+//         //     ans.add(st2.pop());
+//         // }
+//         Collections.reverse(ans);
+//         return ans;
+//     }
+// }
