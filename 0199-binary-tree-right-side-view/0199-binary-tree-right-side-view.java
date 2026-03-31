38bd63925cb060a14dac69516c112dcd8f3f7f05
Pari Goyal <112866509+PariGoyal27@users.noreply.github.com>
2026-03-31 23:20:42 +0530

    Time: 0 ms (100%), Space: 43.5 MB (76.94%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -13,36 +13,54 @@
  *     }
  * }
  */
-class Pair{
-    TreeNode node;
-    int row;
-    Pair(TreeNode node, int row){
-        this.node = node;
-        this.row = row;
-    }
-}
 class Solution {
+    List<Integer> ans;
+    private void solve(TreeNode node, int level){
+        if(node == null) return;
+
+        if(level == ans.size())
+            ans.add(node.val);
+
+        solve(node.right, level+1);
+        solve(node.left, level+1);
+    }
     public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> ans = new ArrayList<>();
-        if(root == null) return ans;
-        TreeMap<Integer, Integer> map = new TreeMap<>();
-        Queue<Pair> q = new LinkedList<>();
-        q.offer(new Pair(root, 0));
-
-        while(!q.isEmpty()){
-            Pair p = q.poll();
-            TreeNode node = p.node;
-            int x = p.row;
-
-            map.put(x, node.val);
-            if(node.left != null)
-                q.offer(new Pair(node.left, x+1));
-            if(node.right != null)
-                q.offer(new Pair(node.right, x+1));
-        }
-        for(Integer e : map.values()){
-            ans.add(e);
-        }
+        ans = new ArrayList<>();
+        solve(root, 0);
         return ans;
     }
 }
+// Iterative
+// class Pair{
+//     TreeNode node;
+//     int row;
+//     Pair(TreeNode node, int row){
+//         this.node = node;
+//         this.row = row;
+//     }
+// }
+// class Solution {
+//     public List<Integer> rightSideView(TreeNode root) {
+//         List<Integer> ans = new ArrayList<>();
+//         if(root == null) return ans;
+//         TreeMap<Integer, Integer> map = new TreeMap<>();
+//         Queue<Pair> q = new LinkedList<>();
+//         q.offer(new Pair(root, 0));
+
+//         while(!q.isEmpty()){
+//             Pair p = q.poll();
+//             TreeNode node = p.node;
+//             int x = p.row;
+
+//             map.put(x, node.val);
+//             if(node.left != null)
+//                 q.offer(new Pair(node.left, x+1));
+//             if(node.right != null)
+//                 q.offer(new Pair(node.right, x+1));
+//         }
+//         for(Integer e : map.values()){
+//             ans.add(e);
+//         }
+//         return ans;
+//     }
+// }
