7e6f4ae27c2038c348ddd9b1bf897892c39918fb
Pari Goyal <112866509+PariGoyal27@users.noreply.github.com>
2026-04-03 13:21:19 +0530

    Time: 1 ms (100%), Space: 48.1 MB (87.39%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.java
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.java
@@ -14,25 +14,44 @@
  * }
  */
 class Solution {
-    int ans = 0;
+    int ans = 0, maxDepth = 0;
     public int deepestLeavesSum(TreeNode root) {
-        // asking for : Last level nodes sum
-        int depth = maxDepth(root);
-        return solve(root, depth);
-    }
-    private int solve(TreeNode root, int depth){
         if(root == null) return 0;
-        if(depth == 1){
-            return root.val;
+        solve(root, 1);
+        return ans;
+    }
+    private void solve(TreeNode root, int depth){
+        if(root == null) return;
+
+        if(depth > maxDepth){
+            maxDepth = depth;
+            ans = root.val;
         }
-        int left = solve(root.left, depth-1);
-        int right = solve(root.right, depth-1);
-        return left + right;
-    }
-    private int maxDepth(TreeNode root){
-        if(root == null) return 0;
-        int left = maxDepth(root.left);
-        int right = maxDepth(root.right);
-        return 1 + Math.max(left, right);
+        else if(depth == maxDepth){
+            ans += root.val;
+        }
+        solve(root.left, depth + 1);
+        solve(root.right, depth + 1);
     }
 }
+// class Solution {
+//     public int deepestLeavesSum(TreeNode root) {
+//         int depth = maxDepth(root);
+//         return solve(root, depth);
+//     }
+//     private int solve(TreeNode root, int depth){
+//         if(root == null) return 0;
+//         if(depth == 1){
+//             return root.val;
+//         }
+//         int left = solve(root.left, depth-1);
+//         int right = solve(root.right, depth-1);
+//         return left + right;
+//     }
+//     private int maxDepth(TreeNode root){
+//         if(root == null) return 0;
+//         int left = maxDepth(root.left);
+//         int right = maxDepth(root.right);
+//         return 1 + Math.max(left, right);
+//     }
+// }
