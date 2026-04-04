026f0fc458b413cd15bccbc235444d570a5fbbc2
Pari Goyal <112866509+PariGoyal27@users.noreply.github.com>
2026-04-04 08:19:04 +0530

    Time: 0 ms (100%), Space: 49.6 MB (37.18%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -13,15 +13,47 @@
  *     }
  * }
  */
+
+// O((logN)^2)
 class Solution {
-    private int count(TreeNode node){
-        if(node == null) return 0;
-        int left = count(node.left);
-        int right = count(node.right);
-        return 1 + left + right;
-    }
     public int countNodes(TreeNode root) {
         if(root == null) return 0;
-        return count(root);
+        
+        int lh = getLeftHeight(root);
+        int rh = getRightHeight(root);
+
+        if(lh == rh) return (1 << lh) - 1;
+
+        return 1 + countNodes(root.left) + countNodes(root.right);
+    }
+    private int getLeftHeight(TreeNode root){
+        int cnt = 1;
+        while(root.left !=  null){
+            cnt++;
+            root = root.left;
+        }
+        return cnt;
+    }
+    private int getRightHeight(TreeNode root){
+        int cnt = 1;
+        while(root.right !=  null){
+            cnt++;
+            root = root.right;
+        }
+        return cnt;
     }
 }
+
+// O(N) Solution
+// class Solution {
+//     private int count(TreeNode node){
+//         if(node == null) return 0;
+//         int left = count(node.left);
+//         int right = count(node.right);
+//         return 1 + left + right;
+//     }
+//     public int countNodes(TreeNode root) {
+//         if(root == null) return 0;
+//         return count(root);
+//     }
+// }
