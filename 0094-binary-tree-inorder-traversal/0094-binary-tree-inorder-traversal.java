d9e9685eb8bfaccf349fa616ff93cb5bc05d8280
Pari Goyal <112866509+PariGoyal27@users.noreply.github.com>
2026-04-06 20:24:02 +0530

    Time: 0 ms (100%), Space: 43.2 MB (58.86%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -13,34 +13,63 @@
  *     }
  * }
  */
-class Solution {
-    private List<Integer> ans;
-    //Recursive
-    // private void inorder(TreeNode node){
-    //     if(node == null) return;
-    //     inorder(node.left);
-    //     ans.add(node.val);
-    //     inorder(node.right);
-    // }
+ //Inorder - Using Threaded Binary tree - Morris Traversal
+ class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
-        ans = new ArrayList<>();
-        // inorder(root);
+        List<Integer> ans = new ArrayList<>();
         if(root == null) return ans;
-        //Iterative
-        Stack<TreeNode> st = new Stack<>();
-        TreeNode node = root;
-
-        while(true){
-            if(node != null){
-                st.push(node);
-                node = node.left;
+        TreeNode curr = root;
+        while(curr != null){
+            if(curr.left == null){
+                ans.add(curr.val);
+                curr = curr.right;
             }else{
-                if(st.isEmpty()) break;
-                node = st.pop();
-                ans.add(node.val);
-                node = node.right;
+                TreeNode prev = curr.left;
+                while(prev.right != null && prev.right != curr){
+                    prev = prev.right;
+                }
+                if(prev.right == null){
+                    prev.right = curr; // Creating thread
+                    curr = curr.left; // moving left
+                }else{
+                    prev.right = null; // removing thread
+                    ans.add(curr.val);
+                    curr = curr.right; // curr can move right now
+                }
             }
         }
         return ans;
     }
 }
+// Recursive and Iterative
+// class Solution {
+//     private List<Integer> ans;
+//     //Recursive
+//     // private void inorder(TreeNode node){
+//     //     if(node == null) return;
+//     //     inorder(node.left);
+//     //     ans.add(node.val);
+//     //     inorder(node.right);
+//     // }
+//     public List<Integer> inorderTraversal(TreeNode root) {
+//         ans = new ArrayList<>();
+//         // inorder(root);
+//         if(root == null) return ans;
+//         //Iterative
+//         Stack<TreeNode> st = new Stack<>();
+//         TreeNode node = root;
+
+//         while(true){
+//             if(node != null){
+//                 st.push(node);
+//                 node = node.left;
+//             }else{
+//                 if(st.isEmpty()) break;
+//                 node = st.pop();
+//                 ans.add(node.val);
+//                 node = node.right;
+//             }
+//         }
+//         return ans;
+//     }
+// }
