c9566875742a3093c4efa6180e4c67101a12e866
Pari Goyal <112866509+PariGoyal27@users.noreply.github.com>
2026-04-06 20:30:31 +0530

    Time: 0 ms (100%), Space: 42.4 MB (99.77%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -13,27 +13,56 @@
  *     }
  * }
  */
-class Solution {
-    List<Integer> ans;
-    //Recursive
-    // private void preorder(TreeNode node){
-    //     if(node == null) return;
-    //     ans.add(node.val);
-    //     preorder(node.left);
-    //     preorder(node.right);
-    // }
-    public List<Integer> preorderTraversal(TreeNode root) {
-        ans = new ArrayList<>();
-        // preorder(root);
+// Preorder - Using threaded binary tree
+class Solution{
+    public List<Integer> preorderTraversal(TreeNode root){
+        List<Integer> ans = new ArrayList<>();
         if(root == null) return ans;
-        Stack<TreeNode> st = new Stack<>();
-        st.push(root);
-        while(!st.isEmpty()){
-            TreeNode top = st.pop();
-            if(top.right != null) st.push(top.right);
-            if(top.left != null) st.push(top.left);
-            ans.add(top.val);
+        TreeNode curr = root;
+        while(curr != null){
+            if(curr.left == null){
+                ans.add(curr.val);
+                curr = curr.right;
+            }else{
+                TreeNode prev = curr.left;
+                while(prev.right != null && prev.right != curr){
+                    prev = prev.right;
+                }
+                if(prev.right == null){
+                    prev.right = curr;
+                    ans.add(curr.val); // when you have marked a thread
+                    curr = curr.left;
+                }else{
+                    prev.right = null; //remove thread
+                    curr = curr.right; //curr can move right
+                }
+            }
         }
         return ans;
     }
 }
+
+// class Solution {
+//     List<Integer> ans;
+//     //Recursive
+//     // private void preorder(TreeNode node){
+//     //     if(node == null) return;
+//     //     ans.add(node.val);
+//     //     preorder(node.left);
+//     //     preorder(node.right);
+//     // }
+//     public List<Integer> preorderTraversal(TreeNode root) {
+//         ans = new ArrayList<>();
+//         // preorder(root);
+//         if(root == null) return ans;
+//         Stack<TreeNode> st = new Stack<>();
+//         st.push(root);
+//         while(!st.isEmpty()){
+//             TreeNode top = st.pop();
+//             if(top.right != null) st.push(top.right);
+//             if(top.left != null) st.push(top.left);
+//             ans.add(top.val);
+//         }
+//         return ans;
+//     }
+// }
