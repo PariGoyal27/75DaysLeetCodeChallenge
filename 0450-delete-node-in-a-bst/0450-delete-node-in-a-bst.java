99e475326049ad2b309f9887bf2dcafe3a15a908
Pari Goyal <112866509+PariGoyal27@users.noreply.github.com>
2026-04-07 12:07:13 +0530

    Time: 0 ms (100%), Space: 47.4 MB (81.87%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -13,43 +13,82 @@
  *     }
  * }
  */
-class Solution {
-    public TreeNode deleteNode(TreeNode root, int key) {
-        if(root == null) return null;
-        if(root.val == key){
-            return helper(root);
+class Solution {    
+    TreeNode findMin(TreeNode root) {
+        while (root.left != null) {
+            root = root.left;
         }
-        TreeNode org = root;
-        while(root != null){
-            if(root.val > key){
-                if(root.left != null && root.left.val == key){
-                    root.left = helper(root.left);
-                    break;
-                }else{
-                    root = root.left;
-                }
-            }
-            else{
-                if(root.right != null && root.right.val == key){
-                    root.right = helper(root.right);
-                    break;
-                }else{
-                    root = root.right;
-                }
-            }
+        return root;
+    }
+    public TreeNode deleteNode(TreeNode root, int key) {        
+        if (root == null)
+            return null;
+
+        if (key < root.val) {
+            root.left = deleteNode(root.left, key);
+        } else if (key > root.val) {
+            root.right = deleteNode(root.right, key);
+        } else {
+            // case 1: no child
+            if (root.left == null && root.right == null)
+                return null;
+
+            // case 2: one child
+            if (root.left == null)
+                return root.right;
+            if (root.right == null)
+                return root.left;
+
+            // case 3: two children
+            TreeNode successor = findMin(root.right);
+            root.val = successor.val;
+            root.right = deleteNode(root.right, successor.val);
         }
-        return org;
-    }
-    private TreeNode helper(TreeNode root){
-        if(root.left == null) return root.right;
-        if(root.right == null) return root.left;
-        TreeNode rightChild = root.right;
-        TreeNode lastRight = findLastRight(root.left);
-        lastRight.right = rightChild;
-        return root.left;
-    }
-    private TreeNode findLastRight(TreeNode root){
-        if(root.right == null) return root;
-        return findLastRight(root.right);
+        return root;
     }
 }
+// class Solution {
+//     public TreeNode deleteNode(TreeNode root, int key) {
+//         if(root == null) return null;
+//         if(root.val == key){
+//             return helper(root);
+//         }
+//         TreeNode org = root;
+//         while(root != null){
+//             if(root.val > key){
+//                 if(root.left != null && root.left.val == key){
+//                     root.left = helper(root.left);
+//                     break;
+//                 }else{
+//                     root = root.left;
+//                 }
+//             }
+//             else{
+//                 if(root.right != null && root.right.val == key){
+//                     root.right = helper(root.right);
+//                     break;
+//                 }else{
+//                     root = root.right;
+//                 }
+//             }
+//         }
+//         return org;
+//     }
+
+//     private TreeNode helper(TreeNode root) {
+//         if (root.left == null)
+//             return root.right;
+//         if (root.right == null)
+//             return root.left;
+//         TreeNode rightChild = root.right;
+//         TreeNode lastRight = findLastRight(root.left);
+//         lastRight.right = rightChild;
+//         return root.left;
+//     }
+
+//     private TreeNode findLastRight(TreeNode root) {
+//         if (root.right == null)
+//             return root;
+//         return findLastRight(root.right);
+//     }
+// }
