7748f08935ba186fff809d5e96b164b704b154f8
Pari Goyal <112866509+PariGoyal27@users.noreply.github.com>
2026-04-09 18:37:59 +0530

    Time: 1 ms (40.73%), Space: 46.8 MB (50.61%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,42 +1,74 @@
-class Pair{
-    int row;
-    int col;
-    Pair(int r, int c){
-        row = r;
-        col = c;
-    }
-}
 class Solution {
+    int[] drow = {-1, 0, 1, 0};
+    int[] dcol = {0, 1, 0, -1};
+    int[][] ans;
+    int m, n;
+    private void dfs(int[][] image, int row, int col, int color, int oldColor){
+        ans[row][col] = color;
+        for(int i = 0; i < 4; i++){
+            int nrow = row + drow[i];
+            int ncol = col + dcol[i];
+            if(nrow >= 0 && nrow < m && ncol >= 0 && ncol < n 
+            && image[nrow][ncol] == oldColor && ans[nrow][ncol] != color){
+                dfs(image, nrow, ncol, color, oldColor);
+            }
+        }
+    }
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int m = image.length;
-        int n = image[0].length;
-        boolean[][] visited = new boolean[m][n];
+        m = image.length;
+        n = image[0].length;
 
         int oldColor = image[sr][sc];
         if(oldColor == color) return image;
 
-        Queue<Pair> q = new LinkedList<>();
-        q.offer(new Pair(sr, sc));
-        image[sr][sc] = color;
-        visited[sr][sc] = true;
-
-        int[] drow = {-1, 0, 1, 0};
-        int[] dcol = {0, 1, 0, -1};
-        while(!q.isEmpty()){
-            int r = q.peek().row;
-            int c = q.peek().col;
-            q.poll();
-            for(int i = 0; i < 4; i++){
-                int nrow = r + drow[i];
-                int ncol = c + dcol[i];
-                if(nrow >= 0 && nrow < m && ncol >= 0 && ncol < n 
-                && !visited[nrow][ncol] && image[nrow][ncol] == oldColor){
-                    image[nrow][ncol] = color;
-                    q.offer(new Pair(nrow, ncol));
-                    visited[nrow][ncol] = true;
-                }
-            }
-        }
-        return image;
+        // Not changing the original data
+        ans = image;
+        
+        dfs(image, sr, sc, color, oldColor);
+        return ans;
     }
 }
+
+//BFS
+// class Pair{
+//     int row;
+//     int col;
+//     Pair(int r, int c){
+//         row = r;
+//         col = c;
+//     }
+// }
+// class Solution {
+//     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+//         int m = image.length;
+//         int n = image[0].length;
+//         boolean[][] visited = new boolean[m][n];
+
+//         int oldColor = image[sr][sc];
+//         if(oldColor == color) return image;
+
+//         Queue<Pair> q = new LinkedList<>();
+//         q.offer(new Pair(sr, sc));
+//         image[sr][sc] = color;
+//         visited[sr][sc] = true;
+
+//         int[] drow = {-1, 0, 1, 0};
+//         int[] dcol = {0, 1, 0, -1};
+//         while(!q.isEmpty()){
+//             int r = q.peek().row;
+//             int c = q.peek().col;
+//             q.poll();
+//             for(int i = 0; i < 4; i++){
+//                 int nrow = r + drow[i];
+//                 int ncol = c + dcol[i];
+//                 if(nrow >= 0 && nrow < m && ncol >= 0 && ncol < n 
+//                 && !visited[nrow][ncol] && image[nrow][ncol] == oldColor){
+//                     image[nrow][ncol] = color;
+//                     q.offer(new Pair(nrow, ncol));
+//                     visited[nrow][ncol] = true;
+//                 }
+//             }
+//         }
+//         return image;
+//     }
+// }
