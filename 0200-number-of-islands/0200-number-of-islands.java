a1a11b0d95d9ff4d50842558ad2f0dcb30c2bd58
Pari Goyal <112866509+PariGoyal27@users.noreply.github.com>
2026-04-10 11:03:07 +0530

    Time: 6 ms (20.37%), Space: 51.4 MB (90.72%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,19 +1,37 @@
+// Using BFS
+class Pair{
+    int row;
+    int col;
+    Pair(int r, int c){
+        row = r;
+        col = c;
+    }
+}
 class Solution {
     boolean[][] visited;
+    int m, n;
     int[] drow = {-1, 0, 1, 0};
     int[] dcol = {0, 1, 0, -1};
-    int m, n;
-    private void dfs(int r, int c, char[][] grid){
-        visited[r][c] = true;
+    private void bfs(int r, int c, char[][] grid){
 
-        for(int i = 0; i < 4; i++){
-            int nrow = r + drow[i];
-            int ncol = c + dcol[i];
-            if(nrow >= 0 && nrow < m && ncol >= 0 && ncol < n
-            && !visited[nrow][ncol] && grid[nrow][ncol] == '1'){
-                dfs(nrow, ncol, grid);
+        Queue<Pair> q = new LinkedList<>();
+        q.offer(new Pair(r, c));
+        visited[r][c] = true;
+        while(!q.isEmpty()){
+            int row = q.peek().row;
+            int col = q.peek().col;
+            q.poll();
+            for(int i = 0; i < 4; i++){
+                int nrow = row + drow[i];
+                int ncol = col + dcol[i];
+                if(nrow >= 0 && nrow < m && ncol >= 0 && ncol < n
+                && !visited[nrow][ncol] && grid[nrow][ncol] == '1'){
+                    visited[nrow][ncol] = true;
+                    q.offer(new Pair(nrow, ncol));
+                }
             }
         }
+
     }
     public int numIslands(char[][] grid) {
         m = grid.length;
@@ -25,7 +43,7 @@ class Solution {
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
                 if(grid[i][j] == '1' && !visited[i][j]){
-                    dfs(i, j, grid);
+                    bfs(i, j, grid);
                     islands++;
                 }
             }
@@ -33,3 +51,39 @@ class Solution {
         return islands;
     }
 }
+// Using DFS
+// class Solution {
+//     boolean[][] visited;
+//     int[] drow = {-1, 0, 1, 0};
+//     int[] dcol = {0, 1, 0, -1};
+//     int m, n;
+//     private void dfs(int r, int c, char[][] grid){
+//         visited[r][c] = true;
+
+//         for(int i = 0; i < 4; i++){
+//             int nrow = r + drow[i];
+//             int ncol = c + dcol[i];
+//             if(nrow >= 0 && nrow < m && ncol >= 0 && ncol < n
+//             && !visited[nrow][ncol] && grid[nrow][ncol] == '1'){
+//                 dfs(nrow, ncol, grid);
+//             }
+//         }
+//     }
+//     public int numIslands(char[][] grid) {
+//         m = grid.length;
+//         n = grid[0].length;
+//         visited = new boolean[m][n];
+
+//         int islands = 0;
+
+//         for(int i = 0; i < m; i++){
+//             for(int j = 0; j < n; j++){
+//                 if(grid[i][j] == '1' && !visited[i][j]){
+//                     dfs(i, j, grid);
+//                     islands++;
+//                 }
+//             }
+//         }
+//         return islands;
+//     }
+// }
