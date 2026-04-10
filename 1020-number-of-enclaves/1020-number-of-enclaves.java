1aeeedcc980ae86006070343cb5a2fe81fc4d503
Pari Goyal <112866509+PariGoyal27@users.noreply.github.com>
2026-04-10 14:49:54 +0530

    Time: 8 ms (69.82%), Space: 59 MB (61.94%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -3,19 +3,17 @@ class Solution {
     int m, n;
     int[] drow = {-1, 0, 1, 0};
     int[] dcol = {0, 1, 0, -1};
-    private int dfs(int r, int c, int[][] grid){
+    private void dfs(int r, int c, int[][] grid){
         visited[r][c] = true;
 
-        int area = 1;
         for(int i = 0; i < 4; i++){
             int nrow = r + drow[i];
             int ncol = c + dcol[i];
             if(nrow >= 0 && nrow < m && ncol >= 0 && ncol < n 
             && grid[nrow][ncol] == 1 && !visited[nrow][ncol]){
-                area += dfs(nrow, ncol, grid);
+                dfs(nrow, ncol, grid);
             }
         }
-        return area;
     }
     public int numEnclaves(int[][] grid) {
         m = grid.length;
@@ -25,19 +23,16 @@ class Solution {
         //Mark all the boundary lands visited first
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
-                if(i == 0 || j == 0 || i == m-1 || j == n-1){
+                if(i == 0 || j == 0 || i == m-1 || j == n-1)
                     if(grid[i][j] == 1 && !visited[i][j])
                         dfs(i, j, grid); //marking visiteed
-                }
             }
-        }        
-        
+        }
         int area = 0;
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
-                if(grid[i][j] == 1 && !visited[i][j]){
-                    area += dfs(i, j, grid);
-                }
+                if(grid[i][j] == 1 && !visited[i][j])
+                    area++;
             }
         }
         return area;
