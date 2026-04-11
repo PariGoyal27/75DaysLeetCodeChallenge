d6a1e820843024d1b6ab71914c1fa420ed0d99bf
Pari Goyal <112866509+PariGoyal27@users.noreply.github.com>
2026-04-11 11:36:10 +0530

    Time: 4 ms (99.84%), Space: 63.6 MB (26.22%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
@@ -1,5 +1,7 @@
+// Marking safe nodes in a call
 class Solution {
     boolean visited[], pathVis[];
+    boolean mark[];
     private boolean dfs(int node, int[][] graph){
         visited[node] = true;
         pathVis[node] = true;
@@ -9,20 +11,53 @@ class Solution {
             }else if(pathVis[it]) return true;
         }
         pathVis[node] = false;
+        mark[node] = true; // It is a safe node
         return false;
     }
     public List<Integer> eventualSafeNodes(int[][] graph) {
         int V = graph.length;
         visited = new boolean[V];
         pathVis = new boolean[V];
+        mark = new boolean[V];
+        for(int i = 0; i < V; i++){
+            if(!visited[i]){
+                dfs(i, graph);
+            }
+        }
         List<Integer> ans = new ArrayList<>();
         for(int i = 0; i < V; i++){
-            // if(!visited[i]){
-                if(!dfs(i, graph)){
-                    ans.add(i);
-                }
-            // }
+            if(mark[i])
+                ans.add(i);
         }
         return ans;
     }
 }
+// Working fine
+// class Solution {
+//     boolean visited[], pathVis[];
+//     private boolean dfs(int node, int[][] graph){
+//         visited[node] = true;
+//         pathVis[node] = true;
+//         for(int it : graph[node]){
+//             if(!visited[it]){
+//                 if(dfs(it, graph)) return true;
+//             }else if(pathVis[it]) return true;
+//         }
+//         pathVis[node] = false;
+//         return false;
+//     }
+//     public List<Integer> eventualSafeNodes(int[][] graph) {
+//         int V = graph.length;
+//         visited = new boolean[V];
+//         pathVis = new boolean[V];
+//         List<Integer> ans = new ArrayList<>();
+//         for(int i = 0; i < V; i++){
+//             if(!pathVis[i]){
+//                 if(!dfs(i, graph)){
+//                     ans.add(i);
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// }
