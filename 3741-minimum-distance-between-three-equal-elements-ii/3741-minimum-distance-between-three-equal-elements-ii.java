17820baa2131e65b76b6686c59fbe49379ce3623
Pari Goyal <112866509+PariGoyal27@users.noreply.github.com>
2026-04-11 06:33:20 +0530

    Time: 6 ms (100%), Space: 161 MB (100%) - LeetHub

--- 3741-minimum-distance-between-three-equal-elements-ii/3741-minimum-distance-between-three-equal-elements-ii.java
+++ 3741-minimum-distance-between-three-equal-elements-ii/3741-minimum-distance-between-three-equal-elements-ii.java
@@ -3,19 +3,43 @@ class Solution {
         int minDist = Integer.MAX_VALUE;
         int n = nums.length;
         if(n <= 2) return -1;
-        HashMap<Integer, List<Integer>> map = new HashMap<>();
+        int[] prev1 = new int[n+1];
+        int[] prev2 = new int[n+1];
+        Arrays.fill(prev1, -1);
+        Arrays.fill(prev2, -1);
         for(int i = 0; i < n; i++){
-            if(!map.containsKey(nums[i]))
-                map.put(nums[i], new ArrayList<>());
-            map.get(nums[i]).add(i);
-        }
-        for(List<Integer> list : map.values()){
-            if(list.size() < 3) continue;
-            
-            for(int i = 2; i < list.size(); i++){
-                minDist = Math.min(minDist, 2 * (list.get(i) - list.get(i-2)));
+            // we want to get the first and the last occured index
+            int val = nums[i];
+            // prev2 is -1 means the occurance of val is < 2
+            // third occurance
+            if(prev2[val] != -1){ // if not -1 that means two occurances are done this is third
+                minDist = Math.min(minDist, i - prev2[val]);
             }
+            // second occrance - prev1(first occ) goes in prev2 and second occ goes in prev1 (next line)
+            prev2[val] = prev1[val];
+            // first occurance - goes in prev1
+            prev1[val] = i;
         }
-        return minDist == Integer.MAX_VALUE ? -1 : minDist;
+        return minDist == Integer.MAX_VALUE ? -1 : 2 * minDist;
     }
+    // 117ms
+    // public int minimumDistance(int[] nums) {
+    //     int minDist = Integer.MAX_VALUE;
+    //     int n = nums.length;
+    //     if(n <= 2) return -1;
+    //     HashMap<Integer, List<Integer>> map = new HashMap<>();
+    //     for(int i = 0; i < n; i++){
+    //         if(!map.containsKey(nums[i]))
+    //             map.put(nums[i], new ArrayList<>());
+    //         map.get(nums[i]).add(i);
+    //     }
+    //     for(List<Integer> list : map.values()){
+    //         if(list.size() < 3) continue;
+            
+    //         for(int i = 2; i < list.size(); i++){
+    //             minDist = Math.min(minDist, 2 * (list.get(i) - list.get(i-2)));
+    //         }
+    //     }
+    //     return minDist == Integer.MAX_VALUE ? -1 : minDist;
+    // }
 }
