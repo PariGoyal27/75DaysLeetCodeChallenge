95f3f763f6369aed201ca45d21faf5bbf1c81999
Pari Goyal <112866509+PariGoyal27@users.noreply.github.com>
2026-04-11 07:42:56 +0530

    Time: 0 ms (100%), Space: 44.6 MB (96.69%) - LeetHub

---
+++ 0112-path-sum/0112-path-sum.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private boolean isLeaf(TreeNode root){
+        return root.left == null && root.right == null;
+    }
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if(root == null) return false;
+        targetSum -= root.val;
+        if(isLeaf(root))
+            return targetSum == 0;
+
+        return hasPathSum(root.left, targetSum) || hasPathSum(root.right, targetSum);
+    }
+}
