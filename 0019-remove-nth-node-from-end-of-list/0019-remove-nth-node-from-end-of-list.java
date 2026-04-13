720142145d229b9da74747e008d90ce03852a8ea
Pari Goyal <112866509+PariGoyal27@users.noreply.github.com>
2026-04-13 08:02:35 +0530

    Time: 0 ms (100%), Space: 43.5 MB (45.1%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -11,21 +11,43 @@
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
         if(head == null) return head;
-        ListNode slow = head;
-        ListNode fast = head;
-        for(int i = 0; i < n && fast != null; i++){
-            fast = fast.next;
+        ListNode temp = head;
+        int size = 1;
+        while(temp.next != null){
+            size++;
+            temp = temp.next;
         }
-        if(fast == null){
-            // head needs to be deleted
-            return head.next;
+        // Head needs to be deleted
+        if(size == n) return head.next;
+        
+        temp = head;
+        // reach just before the node to be deleted
+        for(int i = 1; i < size - n; i++){
+            temp = temp.next;
         }
-        while(fast.next != null){
-            slow = slow.next;
-            fast = fast.next;
-        }
-        //Now slow is just before the node to be deleted
-        slow.next = slow.next.next;
+       
+        temp.next = temp.next.next;
+        // temp.next = first.next;
         return head;
     }   
+    // Using slow and fast pointer
+    // public ListNode removeNthFromEnd(ListNode head, int n) {
+    //     if(head == null) return head;
+    //     ListNode slow = head;
+    //     ListNode fast = head;
+    //     for(int i = 0; i < n && fast != null; i++){
+    //         fast = fast.next;
+    //     }
+    //     if(fast == null){
+    //         // head needs to be deleted
+    //         return head.next;
+    //     }
+    //     while(fast.next != null){
+    //         slow = slow.next;
+    //         fast = fast.next;
+    //     }
+    //     //Now slow is just before the node to be deleted
+    //     slow.next = slow.next.next;
+    //     return head;
+    // }  
 }
