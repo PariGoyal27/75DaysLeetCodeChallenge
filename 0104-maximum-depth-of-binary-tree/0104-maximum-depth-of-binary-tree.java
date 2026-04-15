befb65a157553c3aa1f8525bb51c8a3895ea8df4
Pari Goyal <112866509+PariGoyal27@users.noreply.github.com>
2026-04-15 07:57:03 +0530

    Time: 0 ms (100%), Space: 44.6 MB (33.47%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -14,16 +14,12 @@
  * }
  */
 class Solution {
-    private int solve(TreeNode node){
-        if(node == null){
-            return 0;
-        }
-        int left = 1 + solve(node.left);
-        int right = 1 + solve(node.right);
-
-        return Math.max(left, right);
-    }
     public int maxDepth(TreeNode root) {
-        return solve(root);
+        if(root == null) return 0;
+
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        
+        return 1 + Math.max(left, right);
     }
 }
