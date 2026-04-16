b826e41833f49b56fe2fc3afbd87dd35a54cf2e7
Pari Goyal <112866509+PariGoyal27@users.noreply.github.com>
2026-04-16 21:25:35 +0530

    Time: 24 ms (97.09%), Space: 44.6 MB (11.19%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.java
@@ -0,0 +1,40 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    private Node dfs(Node node, HashMap<Node, Node> map){
+        if(map.containsKey(node)) 
+            return map.get(node);
+
+        Node newNode = new Node(node.val);
+        map.put(node, newNode); // orgNode -> cloneNode
+
+        for(Node neigh : node.neighbors){
+            newNode.neighbors.add(dfs(neigh, map));
+        }
+        return newNode;
+    }
+    public Node cloneGraph(Node node) {
+        if(node == null) return null;
+
+        HashMap<Node, Node> map = new HashMap<>();
+        return dfs(node, map);
+    }
+}
