2a756510a120c70da61a9e355d7c5d979af25a22
Pari Goyal <112866509+PariGoyal27@users.noreply.github.com>
2026-04-17 06:41:22 +0530

    Time: 3 ms (67.99%), Space: 46.4 MB (58.78%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
@@ -16,11 +16,13 @@
 class Solution {
     private boolean isSame(TreeNode p, TreeNode q){
         if(p == null || q == null) return p == q;
-        return p.val == q.val && isSame(p.left, q.left) && isSame(p.right, q.right); 
+
+        return p.val == q.val && isSame(p.left, q.left) && isSame(p.right, q.right);
     }
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
         if(root == null) return root == subRoot;
         if(isSame(root, subRoot)) return true;
+
         return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
     }
 }
