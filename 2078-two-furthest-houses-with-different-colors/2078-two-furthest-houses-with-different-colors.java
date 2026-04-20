dbf19e1dd3662dc3f36973e40bfcb7e61f38709a
Pari Goyal <112866509+PariGoyal27@users.noreply.github.com>
2026-04-20 08:15:15 +0530

    Time: 0 ms (100%), Space: 43.3 MB (45.54%) - LeetHub

--- 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.java
+++ 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.java
@@ -3,13 +3,24 @@ class Solution {
         int n = colors.length;
         int maxDist = 0;
         for(int i = 0; i < n-1; i++){
-            for(int j = n-1; j > i; j--){
-                if(colors[i] != colors[j]){
-                    maxDist = Math.max(maxDist, j-i);
-                    break;// inner loop break
-                }
+            if(colors[n-1] != colors[i]){
+                maxDist = n-i-1;
+                break;
             }
         }
+        for(int i = n-1; i > 0; i--){
+            if(colors[0] != colors[i]){
+                maxDist = Math.max(maxDist, i);
+            }
+        }
+        // for(int i = 0; i < n-1; i++){
+        //     for(int j = n-1; j > i; j--){
+        //         if(colors[i] != colors[j]){
+        //             maxDist = Math.max(maxDist, j-i);
+        //             break;// inner loop break
+        //         }
+        //     }
+        // }
         return maxDist;
     }
 }
