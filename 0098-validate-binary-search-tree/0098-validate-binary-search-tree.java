3204d18979c2599c7d24a8a881093fbb435d93e8
Pari Goyal <112866509+PariGoyal27@users.noreply.github.com>
2026-04-22 07:14:24 +0530

    Time: 0 ms (100%), Space: 45.4 MB (23.36%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -14,15 +14,12 @@
  * }
  */
 class Solution {
-    private boolean isValid(TreeNode root, int min, int max){
+    private boolean isValid(TreeNode root, long min, long max){
         if(root == null) return true;
         if(root.val <= min || root.val >= max) return false;
         return isValid(root.left, min, root.val) && isValid(root.right, root.val, max);
     }
     public boolean isValidBST(TreeNode root) {
-        if(root == null) return true;
-        int min = Integer.MIN_VALUE;
-        int max = Integer.MAX_VALUE;
-        return isValid(root, min, max);
+        return isValid(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
 }
