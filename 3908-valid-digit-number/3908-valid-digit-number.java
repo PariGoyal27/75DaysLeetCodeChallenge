864906c32ad0a75d9b3818e7e1ed209a9552e760
Pari Goyal <112866509+PariGoyal27@users.noreply.github.com>
2026-04-26 07:37:04 +0530

    Time: 1 ms (100%), Space: 42.5 MB (50%) - LeetHub

--- 3908-valid-digit-number/3908-valid-digit-number.java
+++ 3908-valid-digit-number/3908-valid-digit-number.java
@@ -1,10 +1,19 @@
 class Solution {
     public boolean validDigit(int n, int x) {
         String s = Integer.toString(n);
-        if(s.charAt(0) - '0' == x) return false;
-        for(int i = 1; i < s.length(); i++){
-            if(s.charAt(i) - '0' == x) return true;
-        }
-        return false;
+        char ch = (char)(x + '0');
+        int idx = s.indexOf(ch);
+        if(idx == 0 || idx == -1) return false;
+        return true;
     }
 }
+// class Solution {
+//     public boolean validDigit(int n, int x) {
+//         String s = Integer.toString(n);
+//         if(s.charAt(0) - '0' == x) return false;
+//         for(int i = 1; i < s.length(); i++){
+//             if(s.charAt(i) - '0' == x) return true;
+//         }
+//         return false;
+//     }
+// }
