b3b2f5dcd606c46ae6a53f2760610acc64e2a4ff
Pari Goyal <112866509+PariGoyal27@users.noreply.github.com>
2026-05-08 08:49:22 +0530

    Time: 3 ms (88.96%), Space: 44.8 MB (88.96%) - LeetHub

--- 3582-generate-tag-for-video-caption/3582-generate-tag-for-video-caption.java
+++ 3582-generate-tag-for-video-caption/3582-generate-tag-for-video-caption.java
@@ -1,28 +1,54 @@
 class Solution {
     public String generateTag(String caption) {
-        int n = caption.length();
+        caption = caption.toLowerCase().trim();// trim removes spaces from start and end
         StringBuilder sb = new StringBuilder();
 
-        if(caption.charAt(0) != '#') sb.append('#');
+        sb.append('#');
 
-        boolean isStart = false;
-        boolean foundFirst = false;
-        for(int i = 0; i < n; i++){
+        int i = 0;
+        int n = caption.length();
+        while(i < n){
+            if(sb.length() == 100) break; 
             char ch = caption.charAt(i);
-            if(ch == ' ' && foundFirst) {
-                isStart = true;
-                continue;
-            }else if(ch == ' ') continue;
-            if(isStart){
+            if(ch == ' '){
+                while(ch == ' '){
+                    i++;
+                    ch = caption.charAt(i);
+                }
                 sb.append(Character.toUpperCase(ch));
-                isStart = false;
             }else{
-                foundFirst = true;
-                sb.append(Character.toLowerCase(ch));
+                sb.append(ch);
             }
+            i++;
         }
-        if(sb.length() > 100)
-            sb.setLength(100);
         return sb.toString();
     }
 }
+// class Solution {
+//     public String generateTag(String caption) {
+//         int n = caption.length();
+//         StringBuilder sb = new StringBuilder();
+
+//         if(caption.charAt(0) != '#') sb.append('#');
+
+//         boolean isStart = false;
+//         boolean foundFirst = false;
+//         for(int i = 0; i < n; i++){
+//             char ch = caption.charAt(i);
+//             if(ch == ' ' && foundFirst) {
+//                 isStart = true;
+//                 continue;
+//             }else if(ch == ' ') continue;
+//             if(isStart){
+//                 sb.append(Character.toUpperCase(ch));
+//                 isStart = false;
+//             }else{
+//                 foundFirst = true;
+//                 sb.append(Character.toLowerCase(ch));
+//             }
+//         }
+//         if(sb.length() > 100)
+//             sb.setLength(100);
+//         return sb.toString();
+//     }
+// }
