66d8af2337951954cbcb212462199c35a97a19f9
Pari Goyal <112866509+PariGoyal27@users.noreply.github.com>
2026-05-12 07:08:19 +0530

    Time: 33 ms (73.95%), Space: 58.7 MB (92.31%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -6,17 +6,17 @@ class Solution {
         for(int i = 0; i < n-2; i++){
             if(i > 0 && nums[i] == nums[i-1]) continue;
 
-            int target = -nums[i];
+            // int target = -nums[i];
             int j = i + 1, k = n-1;
             while(j < k){
-                int sum = nums[j] + nums[k];
-                if(sum == target){
+                int sum = nums[i] + nums[j] + nums[k];
+                if(sum == 0){
                     result.add(Arrays.asList(nums[i], nums[j], nums[k]));
                     j++;
                     k--;
                     while(j < k && nums[j] == nums[j-1]) j++;
                     while(j < k && nums[k] == nums[k+1]) k--;
-                }else if(sum < target){
+                }else if(sum < 0){
                     j++;
                 }else{
                     k--;
