b44f353fc75931ad2e07d283c1b1fef2ae210ad1
Pari Goyal <112866509+PariGoyal27@users.noreply.github.com>
2026-05-13 06:18:32 +0530

    Time: 3 ms (89.22%), Space: 48.3 MB (37.21%) - LeetHub

--- 0495-teemo-attacking/0495-teemo-attacking.java
+++ 0495-teemo-attacking/0495-teemo-attacking.java
@@ -1,19 +1,30 @@
 class Solution {
     public int findPoisonedDuration(int[] timeSeries, int duration) {
         int n = timeSeries.length;
-        int i = timeSeries[0];
+        
         int cnt = 0;
-        for(int j = 0; j < n; j++){
-            int start = timeSeries[j];
-            int end = start + duration - 1;
-            if(start > i){
-                cnt += end - start + 1;
-            }
-            else{
-                cnt += end - i + 1;
-            }
-            i = end + 1;
+        for(int i = 0; i < n-1; i++){
+            cnt += Math.min(duration, timeSeries[i+1] - timeSeries[i]);
         }
-        return cnt;
+        return cnt + duration;
     }
 }
+// class Solution {
+//     public int findPoisonedDuration(int[] timeSeries, int duration) {
+//         int n = timeSeries.length;
+//         int i = timeSeries[0]; // read head
+//         int cnt = 0;
+//         for(int j = 0; j < n; j++){
+//             int start = timeSeries[j];
+//             int end = start + duration - 1;
+//             if(start > i){ // non overlapping
+//                 cnt += end - start + 1;
+//             }
+//             else{ // overlapping
+//                 cnt += end - i + 1;
+//             }
+//             i = end + 1;
+//         }
+//         return cnt;
+//     }
+// }
