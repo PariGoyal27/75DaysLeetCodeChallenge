533a193e010207058867b9f22f7eea0b6acc84d2
Pari Goyal <112866509+PariGoyal27@users.noreply.github.com>
2026-05-14 07:07:22 +0530

    Time: 0 ms (100%), Space: 44.4 MB (91.62%) - LeetHub

--- 2784-check-if-array-is-good/2784-check-if-array-is-good.java
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.java
@@ -1,12 +1,35 @@
+// T.C. -> O(n)
+// S.C. -> O(n)
 class Solution {
     public boolean isGood(int[] nums) {
         int n = nums.length;
         
-        Arrays.sort(nums);
-        for(int i = 0; i < n-1; i++){
-            if(nums[i] != i+1) return false;
+        int[] base = new int[n];
+        for(int i = 0; i < n; i++){
+            if(nums[i] >= n) return false;
+            base[nums[i]]++;
+            if(base[nums[i]] > 2) return false;
         }
-        if(nums[n-1] != n-1) return false;
+
+        if(base[n-1] != 2) return false;
+        for(int i = 1; i < n-1; i++){
+            if(base[i] != 1) return false;
+        }
         return true;
     }
 }
+
+// T.C. -> O(nlogn) 
+// S.C. -> O(1);
+// class Solution {
+//     public boolean isGood(int[] nums) {
+//         int n = nums.length;
+        
+//         Arrays.sort(nums);
+//         for(int i = 0; i < n-1; i++){
+//             if(nums[i] != i+1) return false;
+//         }
+//         if(nums[n-1] != n-1) return false;
+//         return true;
+//     }
+// }
