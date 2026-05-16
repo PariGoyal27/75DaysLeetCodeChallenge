92b142c7f90a660c14658ad74cb26c236dc3d541
Pari Goyal <112866509+PariGoyal27@users.noreply.github.com>
2026-05-16 15:51:09 +0530

    Time: 3 ms (55.96%), Space: 45 MB (75.42%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    List<List<Integer>> ans = new ArrayList<>();
+    private void solve(int[] nums, int ind, List<Integer> temp){
+        if(ind >= nums.length){
+            ans.add(new ArrayList<>(temp));
+            return;
+        }
+        ans.add(new ArrayList<>(temp));
+        for(int i = ind; i < nums.length; i++){
+            if(i > ind && nums[i] == nums[i-1]){
+                continue;
+            }
+            temp.add(nums[i]);
+            solve(nums, i + 1, temp);
+            temp.remove(temp.size() - 1);
+        }
+    }
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        solve(nums, 0, new ArrayList<>());
+        return ans;
+    }
+}
