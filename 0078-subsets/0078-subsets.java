dde4e185ee433a446786b4378f2d918c553fb53c
Pari Goyal <112866509+PariGoyal27@users.noreply.github.com>
2026-05-16 16:05:36 +0530

    Time: 1 ms (88.98%), Space: 44.4 MB (20.35%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,22 +1,45 @@
-class Solution {
-    List<List<Integer>> result;
-    private void solve(int[] nums, int i, int n, List<Integer> list){
-        if(i == n){
-            result.add(new ArrayList<>(list));
-            return;
-        }
-        // not take
-        solve(nums, i + 1, n, list);
-        // take
-        list.add(nums[i]);
-        solve(nums, i + 1, n, list);
-        list.remove(list.size()-1);
-    }
-    public List<List<Integer>> subsets(int[] nums) {
-        result = new ArrayList<>();
-        int n = nums.length;
-        List<Integer> temp = new ArrayList<>();
-        solve(nums, 0, n, temp);
+class Solution{
+    public List<List<Integer>> subsets(int[] nums){
+        List<List<Integer>> result = new ArrayList<>();
+        solve(nums, 0, new ArrayList<>(), result);
         return result;
     }
+
+    public void solve(int[] nums, int ind, List<Integer> list, List<List<Integer>> result){
+        result.add(new ArrayList<>(list));
+
+        for(int i = ind ; i < nums.length ; i++){
+
+            list.add(nums[i]);
+
+            solve(nums, i + 1, list, result);
+
+            list.remove(list.size() - 1);
+        }
+    }
 }
+//t.c = O(n.2^n)
+//s.c = O(n.2^n)
+
+// class Solution {
+//     List<List<Integer>> result;
+//     private void solve(int[] nums, int i, int n, List<Integer> list){
+//         if(i == n){
+//             result.add(new ArrayList<>(list));
+//             return;
+//         }
+//         // not take
+//         solve(nums, i + 1, n, list);
+//         // take
+//         list.add(nums[i]);
+//         solve(nums, i + 1, n, list);
+//         list.remove(list.size()-1);
+//     }
+//     public List<List<Integer>> subsets(int[] nums) {
+//         result = new ArrayList<>();
+//         int n = nums.length;
+//         List<Integer> temp = new ArrayList<>();
+//         solve(nums, 0, n, temp);
+//         return result;
+//     }
+// }
