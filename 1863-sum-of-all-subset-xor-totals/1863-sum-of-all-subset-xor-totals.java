4af5da11822a701f8edc19574a1fb696f8f9f864
Pari Goyal <112866509+PariGoyal27@users.noreply.github.com>
2026-05-17 15:02:28 +0530

    Time: 0 ms (100%), Space: 42.5 MB (75.24%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.java
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.java
@@ -1,17 +1,29 @@
 class Solution {
-    private int sum = 0;
-    private void solve(int[] nums, int ind, int xor){
-        
-        sum += xor;
-            
-        for(int i = ind; i < nums.length; i++){
-            xor = xor ^ nums[i];
-            solve(nums, i + 1, xor);
-            xor = xor ^ nums[i];
+    
+    private int solve(int[] nums, int ind, int xor){        
+        if(ind == nums.length){
+            return xor;
         }
+        return solve(nums, ind + 1, xor ^ nums[ind]) + solve(nums, ind + 1, xor);        
     }
     public int subsetXORSum(int[] nums) {
-        solve(nums, 0, 0);
-        return sum;
+        return solve(nums, 0, 0);
     }
 }
+// class Solution {
+//     private int sum = 0;
+//     private void solve(int[] nums, int ind, int xor){
+        
+//         sum += xor;
+            
+//         for(int i = ind; i < nums.length; i++){
+//             xor = xor ^ nums[i];
+//             solve(nums, i + 1, xor);
+//             xor = xor ^ nums[i];
+//         }
+//     }
+//     public int subsetXORSum(int[] nums) {
+//         solve(nums, 0, 0);
+//         return sum;
+//     }
+// }
