3ffafee54129baa6365fa576762f1acd2f64e2bf
Pari Goyal <112866509+PariGoyal27@users.noreply.github.com>
2026-05-17 06:45:12 +0530

    Time: 1 ms (99.73%), Space: 52.2 MB (51.59%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.java
+++ 1306-jump-game-iii/1306-jump-game-iii.java
@@ -1,15 +1,15 @@
 class Solution {
-    boolean visited[];
-    private boolean solve(int[] arr, int i){
-        if(i < 0 || i >= arr.length) return false;
-        if(arr[i] == 0) return true;
-        if(visited[i]) return false;
-        visited[i] = true;
-        return solve(arr, i + arr[i]) || solve(arr, i - arr[i]);
+    boolean[] vis;
+    private boolean solve(int[] arr, int ind){
+        if(ind >= arr.length || ind < 0) return false;
+        if(arr[ind] == 0) return true;
+        if(vis[ind]) return false;
+
+        vis[ind] = true;
+        return solve(arr, ind + arr[ind]) || solve(arr, ind - arr[ind]);
     }
     public boolean canReach(int[] arr, int start) {
-        int n = arr.length;
-        visited = new boolean[n];
+        vis = new boolean[arr.length];
         return solve(arr, start);
     }
 }
