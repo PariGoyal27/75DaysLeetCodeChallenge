025f43528b7d8e31048974870b1b1c9220cbbf0d
Pari Goyal <112866509+PariGoyal27@users.noreply.github.com>
2026-05-19 13:41:24 +0530

    Time: 2 ms (81.31%), Space: 46.6 MB (63.41%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.java
@@ -0,0 +1,57 @@
+class Solution {
+    List<List<String>> ans = new ArrayList<>();
+
+    public List<List<String>> solveNQueens(int n) {
+        char[][] board = new char[n][n];
+        for(int i = 0; i < n; i++){
+            Arrays.fill(board[i], '.');
+        }
+        solve(0, board, n);
+        return ans;
+    }
+    private void solve(int col, char[][] board, int n){
+        if(col == n){
+            // board is a char 2d array
+            List<String> temp = new ArrayList<>();
+            for(int i = 0; i < board.length; i++){
+                temp.add(new String(board[i]));
+            }
+            ans.add(temp);
+            return;
+        }
+        for(int row = 0; row < n; row++){
+            if(isSafe(row, col, board, n)){
+                board[row][col] = 'Q';
+                solve(col + 1, board, n);
+                board[row][col] = '.';
+            }
+        }
+    }
+    private boolean isSafe(int row, int col, char[][] board, int n){
+        int r = row;
+        int c = col;
+
+        // check in upper diagonal
+        while(row >= 0 && col >= 0){
+            if(board[row][col] == 'Q') return false;
+            row--;
+            col--;
+        }
+        row = r;
+        col = c;
+        // check on left side
+        while(col >= 0){
+            if(board[row][col] == 'Q') return false;
+            col--;
+        }
+        row = r; 
+        col = c;
+        // check lower diagonal
+        while(row < n && col >= 0){
+            if(board[row][col] == 'Q') return false;
+            row++;
+            col--;
+        }
+        return true;
+    }
+}
