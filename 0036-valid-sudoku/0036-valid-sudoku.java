cf70b3d64eb6caafb4ee8554495e9ec416bd9434
Pari Goyal <112866509+PariGoyal27@users.noreply.github.com>
2026-05-19 21:56:58 +0530

    Time: 1 ms (99.94%), Space: 46.2 MB (91.94%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -11,18 +11,24 @@ class Solution {
         return true;
     }
     private boolean isValid(char[][] board, int row, int col, char c){
-        for(int i = 0; i < 9; i++){
-            if(col != i && board[row][i] == c) return false;
-            if(row != i && board[i][col] == c) return false;
-        }
         int rowStart = 3 * (row / 3);
         int colStart = 3 * (col / 3);
-        for(int i = 0; i < 3; i++){
-            for(int j = 0; j < 3; j++){
-                if(rowStart + i != row && colStart + j != col)
-                    if(board[rowStart + i][colStart + j] == c) return false;
-            }
+
+        for(int i = 0; i < 9; i++){
+            // check current row
+            if(col != i && board[row][i] == c) return false;
+            // check current col
+            if(row != i && board[i][col] == c) return false;
+            // check current box
+            if(rowStart + i / 3 != row && colStart + i % 3 != col)
+            if(board[rowStart + i / 3][colStart + i % 3] == c) return false;
         }
+        // for(int i = 0; i < 3; i++){
+        //     for(int j = 0; j < 3; j++){
+        //         if(rowStart + i != row && colStart + j != col)
+        //             if(board[rowStart + i][colStart + j] == c) return false;
+        //     }
+        // }
         return true;
     }
 }
