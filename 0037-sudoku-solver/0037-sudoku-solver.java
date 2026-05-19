506f36a269603109909ee272673689c37e03350e
Pari Goyal <112866509+PariGoyal27@users.noreply.github.com>
2026-05-19 21:53:01 +0530

    Time: 453 ms (5.05%), Space: 42.9 MB (52.22%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,21 +1,21 @@
 class Solution {
     private boolean isValid(char c, char[][] board, int row, int col){
-        // check in the current row
         for(int i = 0; i < 9; i++){
+            // check in the current row
             if(board[row][i] == c) return false;
-        }
-        // check in the current column
-        for(int i = 0; i < 9; i++){
+            // check in the current column
             if(board[i][col] == c) return false;
+            // check in current box
+            if(board[3 *(row / 3) + i / 3][3 * (col / 3) + i % 3] == c) return false;
         }
         // check in the current box
-        int rowStart = 3 * (row / 3);
-        int colStart = 3 * (col / 3);
-        for(int i = 0; i < 3; i++){
-            for(int j = 0; j < 3; j++){
-                if(board[rowStart + i][colStart + j] == c) return false;
-            }
-        }
+        // int rowStart = 3 * (row / 3);
+        // int colStart = 3 * (col / 3);
+        // for(int i = 0; i < 3; i++){
+        //     for(int j = 0; j < 3; j++){
+        //         if(board[rowStart + i][colStart + j] == c) return false;
+        //     }
+        // }
         return true;
     }
     private boolean solve(char[][] board){        
