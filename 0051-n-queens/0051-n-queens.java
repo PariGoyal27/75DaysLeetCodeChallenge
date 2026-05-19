955e5b93b28a847240c1fb0d2d590042cd8d0da2
Pari Goyal <112866509+PariGoyal27@users.noreply.github.com>
2026-05-19 14:01:06 +0530

    Time: 1 ms (99.9%), Space: 46.8 MB (28.12%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -6,10 +6,13 @@ class Solution {
         for(int i = 0; i < n; i++){
             Arrays.fill(board[i], '.');
         }
-        solve(0, board, n);
+        boolean[] leftRow = new boolean[n];
+        boolean[] upperDiag = new boolean[2 * n - 1];
+        boolean[] lowerDiag = new boolean[2 * n - 1];
+        solve(0, board, n, leftRow, upperDiag, lowerDiag);
         return ans;
     }
-    private void solve(int col, char[][] board, int n){
+    private void solve(int col, char[][] board, int n, boolean[] leftRow, boolean[] upperDiag, boolean[] lowerDiag){
         if(col == n){
             // board is a char 2d array
             List<String> temp = new ArrayList<>();
@@ -20,38 +23,75 @@ class Solution {
             return;
         }
         for(int row = 0; row < n; row++){
-            if(isSafe(row, col, board, n)){
+            if(!leftRow[row] && !upperDiag[row + col] && !lowerDiag[n - 1 + col - row]){
                 board[row][col] = 'Q';
-                solve(col + 1, board, n);
+                leftRow[row] = true;
+                upperDiag[row + col] = true;
+                lowerDiag[n - 1 + col - row] = true;
+
+                solve(col + 1, board, n, leftRow, upperDiag, lowerDiag);
+
                 board[row][col] = '.';
+                leftRow[row] = false;
+                upperDiag[row + col] = false;
+                lowerDiag[n - 1 + col - row] = false;
             }
         }
     }
-    private boolean isSafe(int row, int col, char[][] board, int n){
-        int r = row;
-        int c = col;
-
-        // check in upper diagonal
-        while(row >= 0 && col >= 0){
-            if(board[row][col] == 'Q') return false;
-            row--;
-            col--;
-        }
-        row = r;
-        col = c;
-        // check on left side
-        while(col >= 0){
-            if(board[row][col] == 'Q') return false;
-            col--;
-        }
-        row = r; 
-        col = c;
-        // check lower diagonal
-        while(row < n && col >= 0){
-            if(board[row][col] == 'Q') return false;
-            row++;
-            col--;
-        }
-        return true;
-    }
 }
+// class Solution {
+//     List<List<String>> ans = new ArrayList<>();
+
+//     public List<List<String>> solveNQueens(int n) {
+//         char[][] board = new char[n][n];
+//         for(int i = 0; i < n; i++){
+//             Arrays.fill(board[i], '.');
+//         }
+//         solve(0, board, n);
+//         return ans;
+//     }
+//     private void solve(int col, char[][] board, int n){
+//         if(col == n){
+//             // board is a char 2d array
+//             List<String> temp = new ArrayList<>();
+//             for(int i = 0; i < board.length; i++){
+//                 temp.add(new String(board[i]));
+//             }
+//             ans.add(temp);
+//             return;
+//         }
+//         for(int row = 0; row < n; row++){
+//             if(isSafe(row, col, board, n)){
+//                 board[row][col] = 'Q';
+//                 solve(col + 1, board, n);
+//                 board[row][col] = '.';
+//             }
+//         }
+//     }
+//     private boolean isSafe(int row, int col, char[][] board, int n){
+//         int r = row;
+//         int c = col;
+//         // check in upper diagonal
+//         while(row >= 0 && col >= 0){
+//             if(board[row][col] == 'Q') return false;
+//             row--;
+//             col--;
+//         }
+//         row = r;
+//         col = c;
+//         // check on left side
+//         while(col >= 0){
+//             if(board[row][col] == 'Q') return false;
+//             col--;
+//         }
+//         row = r; 
+//         col = c;
+//         // check lower diagonal
+//         while(row < n && col >= 0){
+//             if(board[row][col] == 'Q') return false;
+//             row++;
+//             col--;
+//         }
+//         return true;
+//     }
+// }
