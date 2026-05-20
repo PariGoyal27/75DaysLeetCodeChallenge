16f62388fba07bf2e87ee2c8858962a113d7d00f
Pari Goyal <112866509+PariGoyal27@users.noreply.github.com>
2026-05-20 18:27:43 +0530

    Time: 6 ms (38.14%), Space: 50.1 MB (22.62%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.java
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.java
@@ -1,26 +1,23 @@
 class Solution {
     List<String> ans = new ArrayList<>();
     private void solve(String s, int ind, int parts, String address){
-        if(ind == s.length()){
-            if(parts == 4){
+        if(parts == 4){
+            if(ind == s.length()){
                 // remove last dot
                 String add = address.substring(0, address.length() - 1);
                 ans.add(add);
             }
             return;
         }
-        if(parts > 4) return;
-        for(int i = ind; i < s.length(); i++){
+        for(int i = ind; i < Math.min(ind + 3, s.length()); i++){
             String currStr = s.substring(ind, i + 1);
 
-            if(currStr.length() > 3) break;
-            // length is one
-            if(i == ind){
+            // leading zero for length 2 or 3
+            if(currStr.length() > 1 && currStr.charAt(0) == '0')
+                break;
+            
+            if(Integer.parseInt(currStr) <= 255){
                 solve(s, i + 1, parts + 1, address + currStr + '.');
-            }else{
-                // not leading zero and withing the range
-                if(s.charAt(ind) != '0' && Integer.parseInt(currStr) <= 255)
-                    solve(s, i + 1, parts + 1, address + currStr + '.');
             }
         }
     }
