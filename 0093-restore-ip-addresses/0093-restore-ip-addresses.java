23c50d3f5f2bf171a50ad7286d28dd3f82be614f
Pari Goyal <112866509+PariGoyal27@users.noreply.github.com>
2026-05-20 18:19:14 +0530

    Time: 6 ms (38.14%), Space: 52 MB (5.12%) - LeetHub

---
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.java
@@ -0,0 +1,31 @@
+class Solution {
+    List<String> ans = new ArrayList<>();
+    private void solve(String s, int ind, int parts, String address){
+        if(ind == s.length()){
+            if(parts == 4){
+                // remove last dot
+                String add = address.substring(0, address.length() - 1);
+                ans.add(add);
+            }
+            return;
+        }
+        for(int i = ind; i < s.length(); i++){
+            String currStr = s.substring(ind, i + 1);
+            int curr = Integer.parseInt(currStr);
+
+            if(currStr.length() > 3) break;
+            // length is one
+            if(i == ind){
+                solve(s, i + 1, parts + 1, address + currStr + '.');
+            }else{
+                if(s.charAt(ind) != '0' && curr <= 255)
+                    solve(s, i + 1, parts + 1, address + currStr + '.');
+            }
+        }
+    }
+    public List<String> restoreIpAddresses(String s) {
+        if(s.length() > 12) return ans;
+        solve(s, 0, 0, "");
+        return ans;
+    }
+}
