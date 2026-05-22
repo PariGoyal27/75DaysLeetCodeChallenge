5541dd60a48f35c56ded73e5af413873c9a2cff4
Pari Goyal <112866509+PariGoyal27@users.noreply.github.com>
2026-05-22 07:03:06 +0530

    Time: 0 ms (100%), Space: 43.8 MB (63.65%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,30 +1,23 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int n = nums.length;
-        int l = 0, r = n-1;
-
-        while(l <= r){
-            int mid = l + (r - l) / 2;
-            if(nums[mid] == target) return mid;
-
-            if(nums[mid] < nums[r]){
-                // the right part is sorted
-                //Check if target is exists there or not
-                if(target > nums[mid] && target <= nums[r]){
-                    //target exists in the sorted part
-                    l = mid + 1;
+        int low = 0, high = nums.length - 1;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            if(nums[mid] == target){
+                return mid;
+            }else if(nums[mid] < nums[high]){
+                // right half is sorted
+                if(target > nums[mid] && target <= nums[high]){
+                    low = mid + 1;
                 }else{
-                    //if does not exist there go left
-                    r = mid - 1;
+                    high = mid - 1;
                 }
             }else{
-                // the left part is sorted
-                if(target >= nums[l] && target < nums[mid]){
-                    // target exists in the left sorted part
-                    r = mid - 1;
+                // left half is sorted
+                if(target >= nums[low] && target < nums[mid]){
+                    high = mid - 1;
                 }else{
-                    // target exists on right
-                    l = mid + 1;
+                    low = mid + 1;
                 }
             }
         }
