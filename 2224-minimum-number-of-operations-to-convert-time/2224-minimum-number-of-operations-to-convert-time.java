db5f099f1f03096b2ad2cc11909ea4f5b9757485
Pari Goyal <112866509+PariGoyal27@users.noreply.github.com>
2026-05-25 16:11:35 +0530

    Time: 1 ms (86.15%), Space: 42.6 MB (97.84%) - LeetHub

---
+++ 2224-minimum-number-of-operations-to-convert-time/2224-minimum-number-of-operations-to-convert-time.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int convertTime(String current, String correct) {
+        int h1 = Integer.valueOf(current.substring(0, 2));
+        int m1 = Integer.valueOf(current.substring(3, 5));
+        int h2 = Integer.valueOf(correct.substring(0, 2));
+        int m2 = Integer.valueOf(correct.substring(3, 5));
+
+        if(m1 > m2){
+            h2 -= 1;
+            m2 += 60;
+        }        
+        int req = (h2 - h1) * 60 + m2 - m1;
+
+        int[] mins = {60, 15, 5, 1};
+        int ans = 0;
+        for(int min : mins){
+            ans += req / min;
+            req = req % min;
+            if(req == 0) break;
+        }
+        return ans;
+    }
+}
