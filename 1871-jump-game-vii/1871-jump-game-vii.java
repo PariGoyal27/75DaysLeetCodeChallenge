f021b2d5204be820ef4b50b2bc03929c98ec1e19
Pari Goyal <112866509+PariGoyal27@users.noreply.github.com>
2026-05-25 06:33:45 +0530

    Time: 12 ms (43.69%), Space: 57.6 MB (10.84%) - LeetHub

--- 1871-jump-game-vii/1871-jump-game-vii.java
+++ 1871-jump-game-vii/1871-jump-game-vii.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean canReach(String s, int minJump, int maxJump) {
         int n = s.length();
-        if(s.charAt(n - 1) == '1') return false;
+        if(s.charAt(n-1) == '1') return false;
         boolean[] vis = new boolean[n];
 
         Queue<Integer> q = new LinkedList<>();
@@ -9,13 +9,13 @@ class Solution {
         vis[0] = true;
 
         int farthestProcessed = 0;
+
         while(!q.isEmpty()){
             int ind = q.poll();
             if(ind == n-1) return true;
 
             int start = Math.max(farthestProcessed + 1, ind + minJump);
             int end = Math.min(n - 1, ind + maxJump);
-            
             for(int i = start; i <= end; i++){
                 if(!vis[i] && s.charAt(i) == '0'){
                     if(i == n-1) return true;
@@ -28,25 +28,27 @@ class Solution {
         return false;
     }
 }
+// TLE -> 117/143
 // class Solution {
-//     boolean[] vis;
-//     private boolean solve(String s, int ind, int minJump, int maxJump){
-//         if(ind >= s.length()) return false;
-//         if(s.charAt(ind) == '1') return false;
-//         if(ind == s.length() - 1) return true;
-//         if(vis[ind]) return false;
-//         vis[ind] = true;
-
-//         boolean ans = false;
-//         for(int i = ind + maxJump; i >= ind + minJump; i--){
-//             ans = ans || solve(s, i, minJump, maxJump);
+//     Boolean[] dp;
+//     private boolean solve(String s, int minJump, int maxJump, int i){
+//         if(i == s.length() - 1){
+//             return true;
 //         }
-//         return ans;
+//         if(s.charAt(i) == '1') return false;
+//         if(dp[i] != null) return dp[i];
+
+//         for(int k = minJump; k <= maxJump && i + k < s.length(); k++){
+//             if(s.charAt(i + k) == '0'){
+//                 if(solve(s, minJump, maxJump, i + k)){
+//                     return dp[i] = true;
+//                 }
+//             }
+//         }
+//         return dp[i] = false;
 //     }
 //     public boolean canReach(String s, int minJump, int maxJump) {
-//         int n = s.length();
-//         if(s.charAt(n - 1) == '1') return false;
-//         vis = new boolean[n];
-//         return solve(s, 0, minJump, maxJump);
+//         dp = new Boolean[s.length()];
+//         return solve(s, minJump, maxJump, 0);
 //     }
 // }
