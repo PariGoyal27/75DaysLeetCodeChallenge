bd8bedf9b14b8d1019f904458d3872f547255842
Pari Goyal <112866509+PariGoyal27@users.noreply.github.com>
2026-05-25 19:33:06 +0530

    Time: 4 ms (84.26%), Space: 45 MB (42.81%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,23 +1,32 @@
 class Solution {
+    int[][] dp;
     private int solve(int i, int[] nums, int target){
         if(i == nums.length) {
             if(target == 0) return 1;
             return 0;
         }        
+        if(dp[i][target] != -1) return dp[i][target];
 
-        int plus = solve(i + 1, nums, target - nums[i]);
-        int minus = solve(i + 1, nums, target + nums[i]);
+        int take = 0;
+        if(nums[i] <= target){
+            take = solve(i + 1, nums, target - nums[i]);
+        }
+        int notTake = solve(i + 1, nums, target);
 
-        return plus + minus;
+        return dp[i][target] = take + notTake;
     }
     public int findTargetSumWays(int[] nums, int target) {
         int total = 0;
         for(int num : nums){
             total += num;
         }
-        if(target < -total) return 0;
+        if(total < target || (total - target) % 2 != 0) return 0;
 
-
+        target = (total - target) / 2;
+        // now count subsets with this target sum
+        dp = new int[nums.length][target + 1];
+        for(int[] row : dp)
+            Arrays.fill(row, -1);
         return solve(0, nums, target);
     }
 }
@@ -41,5 +50,29 @@ class Solution {
 //         if(target < -total) return 0;
 
 //         return solve(0, nums, target, 0);
+//     }
+// }
+
+// class Solution {
+//     private int solve(int i, int[] nums, int target){
+//         if(i == nums.length) {
+//             if(target == 0) return 1;
+//             return 0;
+//         }        
+
+//         int plus = solve(i + 1, nums, target - nums[i]);
+//         int minus = solve(i + 1, nums, target + nums[i]);
+
+//         return plus + minus;
+//     }
+//     public int findTargetSumWays(int[] nums, int target) {
+//         int total = 0;
+//         for(int num : nums){
+//             total += num;
+//         }
+//         if(target < -total) return 0;
+
+
+//         return solve(0, nums, target);
 //     }
 // }
