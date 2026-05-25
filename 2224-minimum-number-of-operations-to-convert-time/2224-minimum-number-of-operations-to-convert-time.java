df328393c110e321763d3880b0491192287eea58
Pari Goyal <112866509+PariGoyal27@users.noreply.github.com>
2026-05-25 16:15:32 +0530

    Time: 1 ms (86.15%), Space: 43.5 MB (12.99%) - LeetHub

--- 2224-minimum-number-of-operations-to-convert-time/2224-minimum-number-of-operations-to-convert-time.java
+++ 2224-minimum-number-of-operations-to-convert-time/2224-minimum-number-of-operations-to-convert-time.java
@@ -5,11 +5,12 @@ class Solution {
         int h2 = Integer.valueOf(correct.substring(0, 2));
         int m2 = Integer.valueOf(correct.substring(3, 5));
 
-        if(m1 > m2){
-            h2 -= 1;
-            m2 += 60;
-        }        
-        int req = (h2 - h1) * 60 + m2 - m1;
+        // if(m1 > m2){
+        //     h2 -= 1;
+        //     m2 += 60;
+        // }        
+        // int req = (h2 - h1) * 60 + m2 - m1;
+        int req = (h2 * 60 + m2) - (h1 * 60 + m1);
 
         int[] mins = {60, 15, 5, 1};
         int ans = 0;
