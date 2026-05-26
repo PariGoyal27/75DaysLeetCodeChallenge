571551d9ac9cc6a4189e4b4c80f4ee9a8af5ba2b
Pari Goyal <112866509+PariGoyal27@users.noreply.github.com>
2026-05-26 08:47:00 +0530

    Time: 0 ms (100%), Space: 43.3 MB (82.24%) - LeetHub

--- 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.java
+++ 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.java
@@ -1,23 +1,34 @@
 class Solution {
     public String greatestLetter(String s) {
-        HashSet<Character> hs = new HashSet<>();
-        
-        for(char ch : s.toCharArray()){
-            hs.add(ch);
+        for(int i = 25; i >= 0; i--){
+            int i1 = s.indexOf(i + 'a');
+            int i2 = s.indexOf(i + 'A');
+            if(i1 != -1 && i2 != -1)
+                return String.valueOf((char)(i + 'A'));
         }
-        String max = "";
-        for(int i = 0; i < 26; i++){
-            char lower = (char)(i + 'a');
-            char upper = (char)(i + 'A');
-            if(hs.contains(lower) && hs.contains(upper)){
-                String up = String.valueOf(upper);
-                int res = up.compareTo(max);
-                if(res > 0){
-                    // upper comes after max
-                    max = up;
-                }
-            }
-        }
-        return max;
+        return "";
     }
 }
+// class Solution {
+//     public String greatestLetter(String s) {
+//         HashSet<Character> hs = new HashSet<>();
+        
+//         for(char ch : s.toCharArray()){
+//             hs.add(ch);
+//         }
+//         String max = "";
+//         for(int i = 0; i < 26; i++){
+//             char lower = (char)(i + 'a');
+//             char upper = (char)(i + 'A');
+//             if(hs.contains(lower) && hs.contains(upper)){
+//                 String up = String.valueOf(upper);
+//                 int res = up.compareTo(max);
+//                 if(res > 0){
+//                     // upper comes after max
+//                     max = up;
+//                 }
+//             }
+//         }
+//         return max;
+//     }
+// }
