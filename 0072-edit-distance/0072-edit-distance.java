66efd67e5efd28180f2ceb2a59e9c22d5dfd8b22
Pari Goyal <112866509+PariGoyal27@users.noreply.github.com>
2026-05-29 12:03:50 +0530

    Time: 4 ms (78.61%), Space: 46.4 MB (93.72%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,29 +1,80 @@
+// space optimization
 class Solution {
-    int[][] dp;
-    private int solve(int i, int j, String s1, String s2){
-        if(i < 0 && j < 0) return 0;
-        if(i < 0) return j + 1;
-        if(j < 0) return i + 1;
-
-        if(dp[i][j] != -1) return dp[i][j];
-
-        int delete = 1 + solve(i - 1, j, s1, s2);
-
-        if(s1.charAt(i) == s2.charAt(j)){
-            return dp[i][j] = Math.min(solve(i-1, j-1, s1, s2), delete);
-        }else{
-            int replace = 1 + solve(i-1, j-1, s1, s2);
-            int insert = 1 + solve(i, j-1, s1, s2);
-
-            return dp[i][j] = Math.min(delete, Math.min(insert, replace));
-        }
-    }
     public int minDistance(String word1, String word2) {
         int m = word1.length();
         int n = word2.length();
-        dp = new int[m][n];
-        for(int[] row : dp)
-            Arrays.fill(row, -1);
-        return solve(m-1, n-1, word1, word2);
+        int[] curr = new int[n+1];
+        int[] prev = new int[n+1];
+        
+        for(int j = 0; j <= n; j++){
+            prev[j] = j;
+        }
+        for(int i = 1; i <= m; i++){
+            curr[0] = i;
+            for(int j = 1; j <= n; j++){
+                if(word1.charAt(i-1) == word2.charAt(j-1)){
+                    curr[j] = prev[j-1];
+                }else{
+                    curr[j] = 1 + Math.min(prev[j-1], Math.min(prev[j], curr[j-1]));
+                }
+            }
+            prev = curr.clone();
+        }
+        return prev[n];
     }
 }
+// Tabulation
+// class Solution {
+//     public int minDistance(String word1, String word2) {
+//         int m = word1.length();
+//         int n = word2.length();
+//         int[][] dp = new int[m + 1][n + 1];
+        
+//         for(int i = 0; i <= m; i++){
+//             dp[i][0] = i;
+//         }
+//         for(int j = 0; j <= n; j++){
+//             dp[0][j] = j;
+//         }
+//         for(int i = 1; i <= m; i++){
+//             for(int j = 1; j <= n; j++){
+//                 if(word1.charAt(i-1) == word2.charAt(j-1)){
+//                     dp[i][j] = dp[i-1][j-1];
+//                 }else{
+//                     dp[i][j] = 1 + Math.min(dp[i-1][j-1], Math.min(dp[i-1][j], dp[i][j-1]));
+//                 }
+//             }
+//         }
+//         return dp[m][n];
+//     }
+// }
+// brute approach
+// class Solution {
+//     int[][] dp;
+//     private int solve(int i, int j, String s1, String s2){
+//         if(i < 0 && j < 0) return 0;
+//         if(i < 0) return j + 1;
+//         if(j < 0) return i + 1;
+
+//         if(dp[i][j] != -1) return dp[i][j];
+
+
+//         if(s1.charAt(i) == s2.charAt(j)){
+//             return dp[i][j] = solve(i-1, j-1, s1, s2);
+//         }else{
+//             int delete = 1 + solve(i - 1, j, s1, s2);
+//             int replace = 1 + solve(i-1, j-1, s1, s2);
+//             int insert = 1 + solve(i, j-1, s1, s2);
+
+//             return dp[i][j] = Math.min(delete, Math.min(insert, replace));
+//         }
+//     }
+//     public int minDistance(String word1, String word2) {
+//         int m = word1.length();
+//         int n = word2.length();
+//         dp = new int[m][n];
+//         for(int[] row : dp)
+//             Arrays.fill(row, -1);
+//         return solve(m-1, n-1, word1, word2);
+//     }
+// }
