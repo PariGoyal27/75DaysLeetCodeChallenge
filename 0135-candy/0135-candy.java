528318a493f3d68152dc3dcb04dba5e8af9bd262
Pari Goyal <112866509+PariGoyal27@users.noreply.github.com>
2026-06-25 13:32:40 +0530

    Time: 4 ms (23.26%), Space: 47.8 MB (16.91%) - LeetHub

---
+++ 0135-candy/0135-candy.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int n = ratings.length;
+
+        // Arrays.sort(ratings); we can't sort the array it will not give the correct answer
+        int[] candies = new int[n];
+        Arrays.fill(candies, 1);
+        
+        // compare with the left only
+        for(int i = 1; i < n; i++){
+            if(ratings[i] > ratings[i - 1]){
+                candies[i] = candies[i-1] + 1;
+            }
+        }
+        int total = 0;
+        // compare with the right only
+        for(int i = n-2; i >= 0; i--){
+            if(ratings[i] > ratings[i+1]){
+                candies[i] = Math.max(candies[i], candies[i+1] + 1);
+            }
+            total += candies[i];
+        }
+        total += candies[n-1];
+        return total;
+    }
+}
